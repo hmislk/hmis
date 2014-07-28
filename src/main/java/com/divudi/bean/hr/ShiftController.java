5bf1ef9275e431583568bc896f9396a904b400ba
HNDITDushan <s.t.dushan@gmail.com>
2014-07-28 13:59:29 +0530

    pharmacy summery & Hr shift Change Signed-off-by: HNDITDushan <s.t.dushan@gmail.com>

--- src/main/java/com/divudi/bean/hr/ShiftController.java
+++ src/main/java/com/divudi/bean/hr/ShiftController.java
@@ -93,13 +93,15 @@ public class ShiftController implements Serializable {
         if (getCurrentRoster() == null) {
             sql = "select c from Shift c "
                     + " where c.retired=false "
-                    + " and upper(c.name) like :q"
+                    + " and upper(c.name) like :q "
+                    + " and c.hideShift=false "
                     + " order by c.name";
         } else {
             sql = "select c from Shift c "
-                    + " where c.retired=false"
+                    + " where c.retired=false "
                     + " and c.roster=:rs "
-                    + " and upper(c.name) like :q"
+                    + " and upper(c.name) like :q "
+                    + " and c.hideShift=false "
                     + " order by c.name";
             hm.put("rs", getCurrentRoster());
         }
