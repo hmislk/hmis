c8f66fe6dc3e27f1324586b57fd4714f1cf1b837
safrin <mylifeofjava@gmail.com>
2014-09-02 14:55:13 +0530

    Signed-off-by: safrin <mylifeofjava@gmail.com>

--- src/main/java/com/divudi/bean/common/CreditCompanyDueController.java
+++ src/main/java/com/divudi/bean/common/CreditCompanyDueController.java
@@ -424,7 +424,7 @@ public class CreditCompanyDueController implements Serializable {
             for (PatientEncounter b : lst) {
                 newIns.setTotal(newIns.getTotal() + b.getFinalBill().getNetTotal());
                 newIns.setPaidTotalPatient(newIns.getPaidTotalPatient() + b.getFinalBill().getPaidAmount());
-                newIns.setPaidTotal(newIns.getPaidTotal() + b.getCreditPaidAmount());
+                newIns.setPaidTotal(newIns.getPaidTotal() + b.getPaidByCreditCompany());
             }
 
             institutionEncounters.add(newIns);
