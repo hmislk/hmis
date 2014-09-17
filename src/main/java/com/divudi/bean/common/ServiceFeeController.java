0aa2c4f1dca6fa3e72cdd857d34db8dcdfb09944
lahirumadushanka <lahirumadushanka2100@gmail.com>
2014-09-17 15:14:49 +0530

    Signed-off-by: lahirumadushanka <lahirumadushanka2100@gmail.com>

--- src/main/java/com/divudi/bean/common/ServiceFeeController.java
+++ src/main/java/com/divudi/bean/common/ServiceFeeController.java
@@ -152,6 +152,7 @@ public class ServiceFeeController implements Serializable {
     }
 
     private double calTot() {
+        createCharges();
         double tot = 0.0;
         for (ItemFee i : getCharges()) {
             tot += i.getFee();
