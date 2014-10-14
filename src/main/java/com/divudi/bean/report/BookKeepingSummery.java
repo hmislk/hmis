940827f82cb9d66bde48a42117e1b82348d7c3d0
divudi <desktopmachine 2>
2014-10-14 12:53:11 +0530

    Signed-off-by: divudi <desktopmachine 2>

--- src/main/java/com/divudi/bean/report/BookKeepingSummery.java
+++ src/main/java/com/divudi/bean/report/BookKeepingSummery.java
@@ -512,8 +512,7 @@ public class BookKeepingSummery implements Serializable {
 
         String jpql = "select c.name, i.name, count(bi.bill), sum(bf.feeValue), bf.fee.feeType, bi.bill.billClassType "
                 + " from BillFee bf join bf.billItem bi join bi.item i join i.category c  "
-                + " where bi.bill.institution=:ins "
-                + " and bi.item.department.institution=:ins "
+                + " where bi.item.department.institution=:ins "
                 + " and  bi.bill.billType= :bTp  "
                 + " and  bi.bill.createdAt between :fromDate and :toDate "
                 + " and (bi.bill.paymentMethod = :pm1 "
