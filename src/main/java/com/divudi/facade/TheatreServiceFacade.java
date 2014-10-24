538ca6bec6bc79450b66bf0496a44b8ae0b426e3
HNDITDushan <s.t.dushan@gmail.com>
2014-10-24 14:54:38 +0530

    Theater service issue. Signed-off-by: HNDITDushan <s.t.dushan@gmail.com>

---
+++ src/main/java/com/divudi/facade/TheatreServiceFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.divudi.facade;
+
+import com.divudi.entity.inward.TheatreService;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Sniper 619
+ */
+@Stateless
+public class TheatreServiceFacade extends AbstractFacade<TheatreService> {
+    @PersistenceContext(unitName = "hmisPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public TheatreServiceFacade() {
+        super(TheatreService.class);
+    }
+    
+}
