141770173bb8e86a5bbdec1c848519268c0fa3ce
lahirumadushanka <lahirumadushanka2100@gmail.com>
2014-11-20 12:26:08 +0530

    finalyze report and checked Signed-off-by: lahirumadushanka <lahirumadushanka2100@gmail.com>

--- src/main/java/com/divudi/bean/report/CommonReport.java
+++ src/main/java/com/divudi/bean/report/CommonReport.java
@@ -13,6 +13,7 @@ import com.divudi.data.table.String1Value1;
 import com.divudi.ejb.CommonFunctions;
 import com.divudi.entity.Bill;
 import com.divudi.entity.BillFee;
+import com.divudi.entity.BillItem;
 import com.divudi.entity.BilledBill;
 import com.divudi.entity.CancelledBill;
 import com.divudi.entity.Department;
@@ -65,6 +66,7 @@ public class CommonReport implements Serializable {
     ////////////////////
     List<BillFee> billFees;
     List<Bill> referralBills;
+    List<BillItem> referralBillItems;
 
     ////////////////
     private Institution collectingIns;
@@ -810,6 +812,16 @@ public class CommonReport implements Serializable {
         //  calTot(getRefundedBills());
         return refundedBillsPh;
     }
+
+    public List<BillItem> getReferralBillItems() {
+        return referralBillItems;
+    }
+
+    public void setReferralBillItems(List<BillItem> referralBillItems) {
+        this.referralBillItems = referralBillItems;
+    }
+    
+    
 
     public BillsTotals getInstitutionCancelledBillsOwn() {
         if (cancellededBills == null) {
@@ -2184,7 +2196,7 @@ public class CommonReport implements Serializable {
                 + " order by bi.id";
         m.put("fd", fromDate);
         m.put("td", toDate);
-        referralBills = getBillFacade().findBySQL(jpql, m, TemporalType.TIMESTAMP);
+        referralBillItems = getBillItemFac().findBySQL(jpql, m, TemporalType.TIMESTAMP);
 
     }
     
