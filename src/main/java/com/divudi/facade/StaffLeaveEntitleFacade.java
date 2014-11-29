8b2f02529f3d1d6948edcabf27e2ea057adb2c69
safrin <mylifeofjava@gmail.com>
2014-11-29 19:53:12 +0530

    Signed-off-by: safrin <mylifeofjava@gmail.com>

---
+++ src/main/java/com/divudi/facade/StaffLeaveEntitleFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.divudi.facade;
+
+import com.divudi.entity.hr.StaffLeaveEntitle;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author safrin
+ */
+@Stateless
+public class StaffLeaveEntitleFacade extends AbstractFacade<StaffLeaveEntitle> {
+    @PersistenceContext(unitName = "hmisPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public StaffLeaveEntitleFacade() {
+        super(StaffLeaveEntitle.class);
+    }
+    
+}
