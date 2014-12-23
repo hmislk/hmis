c4cc47146360151d13dd25b6ea8b4269bc354da8
kpasan89 <kpasan89@gmail.com>
2014-12-23 16:40:05 +0530

    Merge branch 'master' of https://divudi@bitbucket.org/divudi/ruhunu.git
    Conflicts:
    	src/main/resources/META-INF/persistence.xml
    	src/main/setup/glassfish-resources.xml
    
    Signed-off-by: kpasan89 <kpasan89@gmail.com>

--- src/main/java/com/divudi/entity/hr/StaffShift.java
+++ src/main/java/com/divudi/entity/hr/StaffShift.java
@@ -87,36 +87,39 @@ public class StaffShift implements Serializable {
     private double multiplyingFactorSalary;
     //Multyiplying factor for Ot is Actual one.there is no substration
     double multiplyingFactorOverTime;
-    double basicPerSecond;
-    double earlyInLogged;
-    double earlyOutLogged;
+    //Varified Data
     double earlyInVarified;
     double earlyOutVarified;
-    double workedWithinTimeFrameLogged;
-    double workedOutSideTimeFrameLogged;
-    double workedTimeLogged;
     double workedWithinTimeFrameVarified;
     double workedOutSideTimeFrameVarified;
     double workedTimeVarified;
     double lateInVarified;
     double lateOutVarified;
-    double lateInLogged;
-    double lateOutLogged;
-    double leavedTime;
-    private double leavedTimeNoPay;
-    double leavedTimeOther;
-    @Column(name = "overTimeFromStartRecordLogged")
-    double extraTimeFromStartRecordLogged;
-    @Column(name = "overTimeFromEndRecordLogged")
-    double extraTimeFromEndRecordLogged;
-    @Column(name = "overTimeCompleteRecordLogged")
-    double extraTimeCompleteRecordLogged;
     @Column(name = "overTimeFromStartRecordVarified")
     double extraTimeFromStartRecordVarified;
     @Column(name = "overTimeFromEndRecordVarified")
     double extraTimeFromEndRecordVarified;
     @Column(name = "overTimeCompleteRecordVarified")
     double extraTimeCompleteRecordVarified;
+    //Logged Data
+    double basicPerSecond;
+    double earlyInLogged;
+    double earlyOutLogged;
+    double workedWithinTimeFrameLogged;
+    double workedOutSideTimeFrameLogged;
+    double workedTimeLogged;
+    double lateInLogged;
+    double lateOutLogged;
+    @Column(name = "overTimeFromStartRecordLogged")
+    double extraTimeFromStartRecordLogged;
+    @Column(name = "overTimeFromEndRecordLogged")
+    double extraTimeFromEndRecordLogged;
+    @Column(name = "overTimeCompleteRecordLogged")
+    double extraTimeCompleteRecordLogged;
+    //Leaved Time
+    double leavedTime;
+    private double leavedTimeNoPay;
+    double leavedTimeOther;
 
     private boolean dayOff;
     private boolean sleepingDay;
@@ -147,8 +150,6 @@ public class StaffShift implements Serializable {
     public void setDayOfWeek(int dayOfWeek) {
         this.dayOfWeek = dayOfWeek;
     }
-    
-    
 
     public boolean isTransChecked() {
         return transChecked;
@@ -853,12 +854,12 @@ public class StaffShift implements Serializable {
         calShiftStartEndTime();
         calDayOfWeek();
     }
-    
-    public void calDayOfWeek(){
-        Calendar cal=Calendar.getInstance();
+
+    public void calDayOfWeek() {
+        Calendar cal = Calendar.getInstance();
         cal.setTime(shiftDate);
-        dayOfWeek=cal.get(Calendar.DAY_OF_WEEK);
-        
+        dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+
     }
 
     public void calShiftStartEndTime() {
