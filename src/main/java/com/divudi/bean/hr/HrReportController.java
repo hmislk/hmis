6689cba8bdd4015049224d3d2a38a1839c83d42e
Ruhunu <ruhunuDump@gmail.com>
2014-12-26 19:16:35 +0530

    Signed-off-by: Ruhunu <ruhunuDump@gmail.com>

--- src/main/java/com/divudi/bean/hr/HrReportController.java
+++ src/main/java/com/divudi/bean/hr/HrReportController.java
@@ -811,7 +811,6 @@ public class HrReportController implements Serializable {
             monthEnd.setExtraDutyDays(fetchExtraDutyDays(stf));
             monthEnd.setLatedays(fetchLateDays(stf));
             monthEnd.setDayoff(fetchDayOff(stf));
-
             monthEndRecords.add(monthEnd);
         }
     }
