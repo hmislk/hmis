57c4d3b3f882c9ff79538e0ee1aaf80fe66b18b0
Ruhunu <ruhunuDump@gmail.com>
2015-01-27 10:58:04 +0530

    Signed-off-by: Ruhunu <ruhunuDump@gmail.com>

--- src/main/java/com/divudi/ejb/HumanResourceBean.java
+++ src/main/java/com/divudi/ejb/HumanResourceBean.java
@@ -1167,8 +1167,8 @@ public class HumanResourceBean {
 
         String sql = "Select s From StaffLeave s"
                 + " where s.retired=false "
-                + " and s.form.retired=false "
-                + " and s.retired=false "
+//                + " and s.form.retired=false "
+//                + " and s.retired=false "
                 + " and s.staff=:st"
                 + " and s.leaveDate=:date";
         HashMap hm = new HashMap();
