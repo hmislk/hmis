6f29ac62e40fc408d6aa03b167b8ebc6619eac8d
Lakmedipro Pvt Ltd <lakmedipro@lakmedi.lakmedi.com>
2015-02-10 07:25:51 +0530

    Signed-off-by: Lakmedipro Pvt Ltd <lakmedipro@lakmedi.lakmedi.com>

--- src/main/webapp/resources/sql/delete_bookings_1.sql
+++ src/main/webapp/resources/sql/delete_bookings_1.sql
@@ -1,1 +1,9 @@
-select distinct dtype from category;
+SET FOREIGN_KEY_CHECKS=0;
+DELETE from pharmaceuticalbillitem where `ID` > 0;
+delete from bill where id > 0;
+delete from billcomponent where id > 0;
+delete from billentry where id > 0;
+delete from billfee where id > 0;
+delete from billnumber where id > 0;
+delete from billsession where id > 0;
+SET FOREIGN_KEY_CHECKS=1;
