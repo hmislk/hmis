9067ce1ca8a6c4bb7449fe3dde465b04b6cc1c73
dilshankanishka <dkphettiarachchi@gmailcom>
2015-03-03 09:47:16 -0800

    Merge branch 'master' of https://divudi@bitbucket.org/divudi/ruhunu.git
    Conflicts:
    	src/main/resources/META-INF/persistence.xml
    	src/main/setup/glassfish-resources.xml
    
    Signed-off-by: dilshankanishka <dkphettiarachchi@gmailcom>

--- src/main/java/com/divudi/data/hr/Month.java
+++ src/main/java/com/divudi/data/hr/Month.java
@@ -11,7 +11,19 @@ package com.divudi.data.hr;
  * @author User
  */
 public enum Month {
-    January(0);
+    January(0),
+    February(1),
+    March(2),
+    April(3),
+    May(4),
+    June(5),
+    July(6),
+    August(7),
+    September(8),
+    October(9),
+    November(10),
+    December(11)
+    ;
 
     private int monthValue;
     private Month() {
