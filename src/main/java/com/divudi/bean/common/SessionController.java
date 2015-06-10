6967f036803493244dca718fa824c7f2e2e7c173
lahirumadushanka <lahirumadushanka2100@gmail.com>
2015-06-10 12:52:18 +0530

    Signed-off-by: lahirumadushanka <lahirumadushanka2100@gmail.com>

--- src/main/java/com/divudi/bean/common/SessionController.java
+++ src/main/java/com/divudi/bean/common/SessionController.java
@@ -226,9 +226,9 @@ public class SessionController implements Serializable, HttpSessionListener {
             return false;
         }
 
-        if (true) {
+        if (false) {
             Calendar calendar = Calendar.getInstance();
-            calendar.set(2015, 05, 17, 23, 59, 59);//2015/june/05/23:00:00
+            calendar.set(2015, 05, 17, 23, 59, 59);//2015/june/17/23:00:00
             calendar.set(Calendar.MILLISECOND, 999);
 
             Date expired = calendar.getTime();
