ffe4f5890aef5b421ad1db29bafb5839a8dc3fd3
HNDITDushan <s.t.dushan@gmail.com>
2016-10-20 10:27:12 +0530

    Hr Privillages Signed-off-by: HNDITDushan <s.t.dushan@gmail.com>

--- src/main/java/com/divudi/bean/common/UserPrivilageController.java
+++ src/main/java/com/divudi/bean/common/UserPrivilageController.java
@@ -296,11 +296,29 @@ public class UserPrivilageController implements Serializable {
 
         TreeNode node9 = new PrivilageNode("Human Resource", tmproot);
         TreeNode node91 = new PrivilageNode("HR Menu", node9, Privileges.Hr);
-        TreeNode node910 = new PrivilageNode("HR Anistration", node91, Privileges.HrAdmin);
-        TreeNode node911 = new PrivilageNode("HR Employee History Reports", node91, Privileges.EmployeeHistoryReport);
-        TreeNode node912 = new PrivilageNode("HR Delete Late Leave", node91, Privileges.hrDeleteLateLeave);
-        TreeNode node913 = new PrivilageNode("HR Salary Generate", node91, Privileges.HrGenerateSalary);
-
+        TreeNode node92 = new PrivilageNode("Working Time", node9);
+        TreeNode node920 = new PrivilageNode("Working Time Menu", node92,Privileges.HrWorkingTime);
+        TreeNode node921 = new PrivilageNode("Roster Table", node92,Privileges.HrRosterTable);
+        TreeNode node922 = new PrivilageNode("Upload Attendence", node92,Privileges.HrUploadAttendance);
+        TreeNode node923 = new PrivilageNode("Analyse Attendence By Roster", node92,Privileges.HrAnalyseAttendenceByRoster);
+        TreeNode node924 = new PrivilageNode("Analyse Attendence By Staff", node92,Privileges.HrAnalyseAttendenceByStaff);
+        TreeNode node93 = new PrivilageNode("Form", node9);
+        TreeNode node930 = new PrivilageNode("Form Menu", node93,Privileges.HrForms);
+        TreeNode node931 = new PrivilageNode("Leave Form", node93,Privileges.HrLeaveForms);
+        TreeNode node932 = new PrivilageNode("Additional Form", node93,Privileges.HrAdditionalForms);
+        TreeNode node94 = new PrivilageNode("HR Salary Advance", node9, Privileges.HrAdvanceSalary);
+        TreeNode node95 = new PrivilageNode("HR Salary Generate", node9, Privileges.HrGenerateSalary);
+        TreeNode node96 = new PrivilageNode("HR Salary Print", node9, Privileges.HrPrintSalary);
+        TreeNode node97 = new PrivilageNode("HR Reports", node9);
+        TreeNode node970 = new PrivilageNode("HR Reports Menu", node97,Privileges.HrReports);
+        TreeNode node971 = new PrivilageNode("HR Reports Level 1", node97,Privileges.HrReportsLevel1);
+        TreeNode node972 = new PrivilageNode("HR Reports Level 2", node97,Privileges.HrReportsLevel2);
+        TreeNode node973 = new PrivilageNode("HR Reports Level 3", node97,Privileges.HrReportsLevel3);
+//        TreeNode node974 = new PrivilageNode("HR Employee History Reports", node97, Privileges.EmployeeHistoryReport);
+        TreeNode node98 = new PrivilageNode("HR Administration", node9);
+        TreeNode node980 = new PrivilageNode("HR Administration Menu", node98, Privileges.HrAdmin);
+        TreeNode node981 = new PrivilageNode("HR Delete Late Leave", node98, Privileges.hrDeleteLateLeave);
+        
         TreeNode node8 = new PrivilageNode("Higheist Accountability", tmproot);
         TreeNode node81 = new PrivilageNode("Change Professional Fee", node8, Privileges.ChangeProfessionalFee);
         TreeNode node82 = new PrivilageNode("Change Professional Fee", node8, Privileges.ChangeCollectingCentre);
