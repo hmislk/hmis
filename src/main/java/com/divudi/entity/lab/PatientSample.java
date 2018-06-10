7bcd5aa56a505d2260f7fcbf8be295a30e8384d7
buddhika_ari <buddhika_ari@arogya.c.api-project-316008881718.internal>
2018-06-10 12:48:55 +0530

    Before Starting on Updating Sample Collection

--- src/main/java/com/divudi/entity/lab/PatientSample.java
+++ src/main/java/com/divudi/entity/lab/PatientSample.java
@@ -5,13 +5,19 @@
  */
 package com.divudi.entity.lab;
 
+import com.divudi.entity.Department;
+import com.divudi.entity.Institution;
+import com.divudi.entity.Item;
 import com.divudi.entity.Patient;
+import com.divudi.entity.WebUser;
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
 
 /**
  *
@@ -25,8 +31,43 @@ public class PatientSample implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @ManyToOne
-    Patient patient;
-    
+    private Patient patient;
+    @ManyToOne
+    private PatientInvestigation patientInvestigation;
+    //Sample Collection
+    private Boolean collected;
+    @ManyToOne
+    private WebUser sampleCollecter;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date sampledAt;
+    private Boolean sampleOutside;
+    private String sampleComments;
+    @ManyToOne
+    private Department sampleDepartment;
+    @ManyToOne
+    private Institution sampleInstitution;
+    //
+    @ManyToOne
+    private Item investigationComponant;
+    @ManyToOne
+    private Item test;
+    @ManyToOne
+    private InvestigationTube tube;
+    @ManyToOne
+    private Machine machine;
+    @ManyToOne
+    private Sample sample;
+            //Cancellation
+    private Boolean cancelled = false;
+    @ManyToOne
+    private WebUser cancelledUser;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date cancelledAt;
+    private String cancellComments;
+    @ManyToOne
+    private Department cancellDepartment;
+    @ManyToOne
+    private Institution cancellInstitution;
     
     
 
@@ -38,6 +79,9 @@ public class PatientSample implements Serializable {
         this.id = id;
     }
 
+    
+    
+    
     @Override
     public int hashCode() {
         int hash = 0;
@@ -61,6 +105,166 @@ public class PatientSample implements Serializable {
     @Override
     public String toString() {
         return "com.divudi.entity.lab.PatientSample[ id=" + id + " ]";
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public PatientInvestigation getPatientInvestigation() {
+        return patientInvestigation;
+    }
+
+    public void setPatientInvestigation(PatientInvestigation patientInvestigation) {
+        this.patientInvestigation = patientInvestigation;
+    }
+
+    public Item getInvestigationComponant() {
+        return investigationComponant;
+    }
+
+    public void setInvestigationComponant(Item investigationComponant) {
+        this.investigationComponant = investigationComponant;
+    }
+
+    public Item getTest() {
+        return test;
+    }
+
+    public void setTest(Item test) {
+        this.test = test;
+    }
+
+    public InvestigationTube getTube() {
+        return tube;
+    }
+
+    public void setTube(InvestigationTube tube) {
+        this.tube = tube;
+    }
+
+    public Machine getMachine() {
+        return machine;
+    }
+
+    public void setMachine(Machine machine) {
+        this.machine = machine;
+    }
+
+    public Sample getSample() {
+        return sample;
+    }
+
+    public void setSample(Sample sample) {
+        this.sample = sample;
+    }
+
+    public Boolean getCollected() {
+        return collected;
+    }
+
+    public void setCollected(Boolean collected) {
+        this.collected = collected;
+    }
+
+    public WebUser getSampleCollecter() {
+        return sampleCollecter;
+    }
+
+    public void setSampleCollecter(WebUser sampleCollecter) {
+        this.sampleCollecter = sampleCollecter;
+    }
+
+    public Date getSampledAt() {
+        return sampledAt;
+    }
+
+    public void setSampledAt(Date sampledAt) {
+        this.sampledAt = sampledAt;
+    }
+
+    public Boolean getSampleOutside() {
+        return sampleOutside;
+    }
+
+    public void setSampleOutside(Boolean sampleOutside) {
+        this.sampleOutside = sampleOutside;
+    }
+
+    public String getSampleComments() {
+        return sampleComments;
+    }
+
+    public void setSampleComments(String sampleComments) {
+        this.sampleComments = sampleComments;
+    }
+
+    public Department getSampleDepartment() {
+        return sampleDepartment;
+    }
+
+    public void setSampleDepartment(Department sampleDepartment) {
+        this.sampleDepartment = sampleDepartment;
+    }
+
+    public Institution getSampleInstitution() {
+        return sampleInstitution;
+    }
+
+    public void setSampleInstitution(Institution sampleInstitution) {
+        this.sampleInstitution = sampleInstitution;
+    }
+
+    public Boolean getCancelled() {
+        return cancelled;
+    }
+
+    public void setCancelled(Boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    public WebUser getCancelledUser() {
+        return cancelledUser;
+    }
+
+    public void setCancelledUser(WebUser cancelledUser) {
+        this.cancelledUser = cancelledUser;
+    }
+
+    public Date getCancelledAt() {
+        return cancelledAt;
+    }
+
+    public void setCancelledAt(Date cancelledAt) {
+        this.cancelledAt = cancelledAt;
+    }
+
+    public String getCancellComments() {
+        return cancellComments;
+    }
+
+    public void setCancellComments(String cancellComments) {
+        this.cancellComments = cancellComments;
+    }
+
+    public Department getCancellDepartment() {
+        return cancellDepartment;
+    }
+
+    public void setCancellDepartment(Department cancellDepartment) {
+        this.cancellDepartment = cancellDepartment;
+    }
+
+    public Institution getCancellInstitution() {
+        return cancellInstitution;
+    }
+
+    public void setCancellInstitution(Institution cancellInstitution) {
+        this.cancellInstitution = cancellInstitution;
     }
     
 }
