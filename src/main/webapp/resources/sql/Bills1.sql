3f9985135be8642f87d87c895a08163007730560
buddhika <buddhika.ari@gmail.com>
2018-11-07 05:27:16 +0530

    SMS Sent Coding Completed. Need to check.

--- src/main/webapp/resources/sql/Bills1.sql
+++ src/main/webapp/resources/sql/Bills1.sql
@@ -1,2 +1,2 @@
 
-select id,`TOTAL`, `NETTOTAL`, `PAIDAMOUNT` ,`PAID`, `BALANCE`,`BILLTYPE` from bill  order by id desc limit 100;
+select id,`TOTAL`, `NETTOTAL`, `PAIDAMOUNT` ,`PAID`, `BALANCE`,`BILLTYPE`, `CASHBALANCE`, `CASHPAID` from bill  order by id desc limit 100;
