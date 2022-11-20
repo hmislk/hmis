9c8bd90e5bde7c69a60025f1e7f96baba73454d9
buddhika <buddhika@vm1.asia-southeast1-b.c.openhmis.internal>
2022-11-20 07:57:07 +0530

    Merge origin/Arogya2022 into Arogya2022
    Conflicts:
    	src/main/java/com/divudi/ejb/CommonFunctions.java

--- src/main/java/com/divudi/bean/common/ApplicationController.java
+++ src/main/java/com/divudi/bean/common/ApplicationController.java
@@ -50,7 +50,7 @@ public class ApplicationController {
     private String subject;
     private String body;
     
-    private boolean hasAwebsiteAsFrontEnd=false;
+    private boolean hasAwebsiteAsFrontEnd=true;
     
     
 
