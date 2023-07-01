e080747efbf764f99a62cfb21aaad5eeb5f21ae3
Senula88 <senulananayakkara88@gmail.com>
2023-07-01 12:25:07 +0530

    Close issue #595 Signed-off-by: Senula88 <senulananayakkara88@gmail.com>

--- src/main/java/com/divudi/bean/report/CashierReportController.java
+++ src/main/java/com/divudi/bean/report/CashierReportController.java
@@ -101,7 +101,26 @@ public class CashierReportController implements Serializable {
     public String navigateToDepartmentAllCashierReport(){
         return "/reportCashier/report_cashier_summery_all";
     }
+    
+    public String navigateToCashierReport(){
+        return "/reportCashier/report_cashier_detailed_by_user_by_reciptno.xhtml";
+    }
+    
+    public String navigateToCashierSummary(){
+        return "/reportCashier/report_cashier_summery_by_user.xhtml";
+    }
+    
+    public String navigateToCashierSummaryUsingReciptNo(){
+        return "/reportCashier/report_cashier_summery_by_user_by_reciptno";
+    }
 
+    public String navigateToAllCashierReportUsingReciptNo(){
+        return "/reportCashier/report_cashier_summery_by_user_by_reciptno.xhtml";
+    }
+    
+    public String navigateToAllCashierSummary(){
+        return "/reportCashier/report_cashier_summery_all_total_only.xhtml";
+    }
     public CashierReportController() {
     }
 
