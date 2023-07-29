8c8ae45d45a2c35e3f33aee149a4a0773e7716b0
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2023-07-29 21:14:46 +0530

    Signed-off-by: Dr M H B Ariyaratne <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/entity/Bill.java
+++ src/main/java/com/divudi/entity/Bill.java
@@ -538,7 +538,7 @@ public class Bill implements Serializable {
         StringBuilder str = new StringBuilder();
 
         // Add the table start, thead and headers
-        str.append("<table class='table table-bordered table-striped'>")
+        str.append("<table class='table table-borderless'>")
                 .append("<thead>")
                 .append("<tr>")
                 .append("<th>Item</th>")
