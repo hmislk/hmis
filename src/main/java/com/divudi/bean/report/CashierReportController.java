da7d6c01b14f7f6767a0b777c33140f3ad9bfe52
Senula88 <senulananayakkara88@gmail.com>
2023-08-28 15:58:19 +0530

    Signed-off-by: Senula88 <senulananayakkara88@gmail.com>

--- src/main/java/com/divudi/bean/report/CashierReportController.java
+++ src/main/java/com/divudi/bean/report/CashierReportController.java
@@ -137,6 +137,36 @@ public class CashierReportController implements Serializable {
 
         return "/reportCashier/report_cashier_summery_all?faces-redirect=true";
     }
+    
+    public String navigateToReportcashierDetailedByUser(){
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        ServletContext servletContext = (ServletContext) context.getExternalContext().getContext();
+
+        String url = request.getRequestURL().toString();
+
+        String ipAddress = request.getRemoteAddr();
+        System.out.println("Start");
+        AuditEvent auditEvent = new AuditEvent();
+        auditEvent.setEventStatus("Started");
+        long duration;
+        Date startTime = new Date();
+        auditEvent.setEventDataTime(startTime);
+        auditEvent.setDepartmentId(sessionController.getDepartment().getId());
+        auditEvent.setInstitutionId(sessionController.getInstitution().getId());
+        auditEvent.setUrl(url);
+        auditEvent.setIpAddress(ipAddress);
+        auditEvent.setWebUserId(sessionController.getLoggedUser().getId());
+        auditEvent.setEventTrigger("navigateToReportcashierDetailedByUser()");
+        auditEventApplicationController.logAuditEvent(auditEvent);
+
+        Date endTime = new Date();
+        duration = endTime.getTime() - startTime.getTime();
+        auditEvent.setEventDuration(duration);
+        auditEvent.setEventStatus("Completed");
+        auditEventApplicationController.logAuditEvent(auditEvent);
+        return "/reportCashier/report_cashier_detailed_by_user.xhtml?faces-redirect=true";
+    }
 
     public String navigateToCashierReport() {
         return "/reportCashier/report_cashier_detailed_by_user_by_reciptno.xhtml";
