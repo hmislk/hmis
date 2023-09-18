86edf13786833b4380b82ce6bdaeac1af3cde0b4
Senula88 <senulananayakkara88@gmail.com>
2023-09-18 14:57:11 +0530

    Closes Issue#1028 Signed-off-by: Senula88 <senulananayakkara88@gmail.com>

--- src/main/java/com/divudi/bean/report/PharmacySaleReport.java
+++ src/main/java/com/divudi/bean/report/PharmacySaleReport.java
@@ -1701,6 +1701,42 @@ public class PharmacySaleReport implements Serializable {
         return "/pharmacy/pharmacy_report_summery_all?faces-redirect=true";
     }
     
+    public String navigatePharmacyReportGrnDetail1() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        ServletContext servletContext = (ServletContext) context.getExternalContext().getContext();
+
+        String url = request.getRequestURL().toString();
+
+        String ipAddress = request.getRemoteAddr();
+
+        AuditEvent auditEvent = new AuditEvent();
+        auditEvent.setEventStatus("Started");
+        long duration;
+        Date startTime = new Date();
+        auditEvent.setEventDataTime(startTime);
+        if (sessionController != null && sessionController.getDepartment() != null) {
+            auditEvent.setDepartmentId(sessionController.getDepartment().getId());
+        }
+
+        if (sessionController != null && sessionController.getInstitution() != null) {
+            auditEvent.setInstitutionId(sessionController.getInstitution().getId());
+        }
+        if (sessionController != null && sessionController.getLoggedUser() != null) {
+            auditEvent.setWebUserId(sessionController.getLoggedUser().getId());
+        }
+        auditEvent.setUrl(url);
+        auditEvent.setIpAddress(ipAddress);
+        auditEvent.setEventTrigger("navigatePharmacyReportGrnDetail1()");
+        auditEventApplicationController.logAuditEvent(auditEvent);
+        Date endTime = new Date();
+        duration = endTime.getTime() - startTime.getTime();
+        auditEvent.setEventDuration(duration);
+        auditEvent.setEventStatus("Completed");
+        auditEventApplicationController.logAuditEvent(auditEvent);
+        return "/pharmacy/pharmacy_report_grn_detail_1?faces-redirect=true";
+    }
+    
     private List<Object[]> fetchSaleValueByPaymentmethodByBill() {
         String sql;
 
