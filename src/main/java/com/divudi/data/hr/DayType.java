425567bf0663ff52463cbf735e7e9224c547d59a
Thiwanka570 <jvptmadushan@gmail.com>
2023-09-22 15:35:40 +0530

    Signed-off-by: Thiwanka570 <jvptmadushan@gmail.com>

--- src/main/java/com/divudi/data/hr/DayType.java
+++ src/main/java/com/divudi/data/hr/DayType.java
@@ -1,31 +1,34 @@
-/*
- * Open Hospital Management Information System
- * Dr M H B Ariyaratne
- * buddhika.ari@gmail.com
- */
-package com.divudi.data.hr;
-
-/**
- *
- * @author safrin
- */
 public enum DayType {
+    Normal("Normal Day"),
+    DayOff("Day Off"),
+    SleepingDay("Sleeping Day"),
+    Poya("Poya Day"),
+    PublicHoliday("Public Holiday"),
+    MurchantileHoliday("Merchantile Holiday"),
+    Weekday("Weekday", true),
+    Saturday("Saturday", true),
+    Sunday("Sunday", true),
+    Holiday("Holiday", true),
+    All("All", true),
+    Extra("Extra Day");
 
-    Normal,
-    DayOff,
-    SleepingDay,
-    Poya,
-    PublicHoliday,
-    MurchantileHoliday,
-    @Deprecated
-    Weekday,
-    @Deprecated
-    Saturday,
-    @Deprecated
-    Sunday,
-    @Deprecated
-    Holiday,
-    @Deprecated
-    All,
-    Extra,
+    private final String label;
+    private final boolean deprecated;
+
+    DayType(String label) {
+        this(label, false);
+    }
+
+    DayType(String label, boolean deprecated) {
+        this.label = label;
+        this.deprecated = deprecated;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public boolean isDeprecated() {
+        return deprecated;
+    }
 }
