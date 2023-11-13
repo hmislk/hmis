0005f3180dbc2d4584a7df9c2c67ae5847b80885
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2023-11-13 08:41:58 +0530

    Signed-off-by: Dr M H B Ariyaratne <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/bean/common/SessionController.java
+++ src/main/java/com/divudi/bean/common/SessionController.java
@@ -1113,7 +1113,10 @@ public class SessionController implements Serializable, HttpSessionListener {
         dashboards = webUserController.listWebUserDashboards(loggedUser);
 
         userPrivilages = fillUserPrivileges(loggedUser, department, false);
+        System.out.println("userPrivilages = " + userPrivilages);
         if (userPrivilages == null || userPrivilages.isEmpty()) {
+            userPrivilages = fillUserPrivileges(loggedUser, null, true);
+            System.out.println("userPrivilages = " + userPrivilages);
             createUserPrivilegesForAllDepartments(loggedUser, department, loggableDepartments);
             logout();
         }
@@ -1517,6 +1520,8 @@ public class SessionController implements Serializable, HttpSessionListener {
         }
         m.put("ret", false);
         m.put("wu", twu);
+        System.out.println("m = " + m);
+        System.out.println("sql = " + sql);
         List<WebUserPrivilege> twups = getWebUserPrivilegeFacade().findByJpql(sql, m);
         return twups;
     }
@@ -1887,6 +1892,7 @@ public class SessionController implements Serializable, HttpSessionListener {
     }
 
     private void createUserPrivilegesForAllDepartments(WebUser tmpLoggedUser, Department loggedDept, List<Department> tmpLoggableDeps) {
+        System.out.println("createUserPrivilegesForAllDepartments");
         List<WebUserPrivilege> twups = fillUserPrivileges(tmpLoggedUser, null, true);
         if (tmpLoggedUser == null) {
             return;
@@ -1901,9 +1907,12 @@ public class SessionController implements Serializable, HttpSessionListener {
             return;
         }
         List<Department> tds = tmpLoggableDeps;
+        System.out.println("tds = " + tds);
         tds.remove(loggedDept);
+        System.out.println("tds = " + tds);
         for (WebUserPrivilege twup : twups) {
             twup.setDepartment(loggedDept);
+            System.out.println("twup = " + twup.getDepartment().getName());
             getWebUserPrivilegeFacade().edit(twup);
             for (Department d : tds) {
                 WebUserPrivilege nwup = new WebUserPrivilege();
