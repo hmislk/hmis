7dc1dfe427876c35178c8adb23a00ab6b6a7d8bf
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2023-11-23 09:13:50 +0530

    Signed-off-by: Dr M H B Ariyaratne <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/ws/common/ApplicationConfig.java
+++ src/main/java/com/divudi/ws/common/ApplicationConfig.java
@@ -36,6 +36,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.divudi.ws.common.ApiMembership.class);
     }
     
 }
