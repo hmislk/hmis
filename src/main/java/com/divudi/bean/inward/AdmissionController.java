ac2c6fed33bc220a420000ceb076e090d09f214b
Senula88 <senulananayakkara88@gmail.com>
2023-11-29 22:02:52 +0530

    Signed-off-by: Senula88 <senulananayakkara88@gmail.com>

--- src/main/java/com/divudi/bean/inward/AdmissionController.java
+++ src/main/java/com/divudi/bean/inward/AdmissionController.java
@@ -316,15 +316,15 @@ public class AdmissionController implements Serializable {
             return;
         }
         
-        if (fromDate != null && fromDate.compareTo(CommonFunctions.getEndOfDay()) >= 0) {
-            UtilityController.addErrorMessage("Please select from date below or equal to the current date");
-            return;
-        }
-        
-        if (toDate != null && toDate.compareTo(CommonFunctions.getEndOfDay()) >= 0) {
-            UtilityController.addErrorMessage("Please select to date below or equal to the current date");
-            return;
-        }
+//        if (fromDate != null && fromDate.compareTo(CommonFunctions.getEndOfDay()) >= 0) {
+//            UtilityController.addErrorMessage("Please select from date below or equal to the current date");
+//            return;
+//        }
+//        
+//        if (toDate != null && toDate.compareTo(CommonFunctions.getEndOfDay()) >= 0) {
+//            UtilityController.addErrorMessage("Please select to date below or equal to the current date");
+//            return;
+//        }
         
         String j;
         HashMap m = new HashMap();
