f800308a93efc282e82e0fb8c57b3f2d5ea4020f
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2024-01-19 18:35:19 +0530

    Closes #2940 Signed-off-by: Dr M H B Ariyaratne <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/bean/common/BillBeanController.java
+++ src/main/java/com/divudi/bean/common/BillBeanController.java
@@ -160,7 +160,6 @@ public class BillBeanController implements Serializable {
 //            return false;
 //        }
 //    }
-
     public DepartmentFacade getDepartmentFacade() {
         return departmentFacade;
     }
@@ -1781,7 +1780,7 @@ public class BillBeanController implements Serializable {
         return getBillFeeFacade().findAggregates(sql, temMap, TemporalType.TIMESTAMP);
 
     }
-    
+
     public List<Object[]> fetchBilledDepartmentItem(Date fromDate, Date toDate, Department department, BillType bt, boolean toDep) {
         String sql;
         Map temMap = new HashMap();
@@ -2800,6 +2799,45 @@ public class BillBeanController implements Serializable {
             deptSet.add(be.getBillItem().getItem().getDepartment());
         }
         return deptSet.size();
+    }
+
+    public void checkBillItemFeesInitiated(Bill b) {
+        if (b == null) {
+            return;
+        }
+        if (b.getBillItems() == null) {
+            b.setBillItems(fillBillItems(b));
+        }
+        if (b.getBillItems() == null || b.getBillItems().isEmpty()) {
+            return;
+        }
+        for (BillItem bi : b.getBillItems()) {
+            if (bi.getBillFees() == null || bi.getBillFees().isEmpty()) {
+                bi.setBillFees(fillBillItemFees(bi));
+            }
+        }
+    }
+
+    public List<BillItem> fillBillItems(Bill b) {
+        String j = "Select bi "
+                + " from BillItem bi "
+                + " where bi.bill=:b "
+                + " and bi.retired=:ret ";
+        Map m = new HashMap();
+        m.put("ret", false);
+        m.put("b", b);
+        return billItemFacade.findByJpql(j, m);
+    }
+
+    public List<BillFee> fillBillItemFees(BillItem bi) {
+        String j = "Select bf "
+                + " from BillFee bf "
+                + " where bf.billItem=:bi "
+                + " and bf.retired=:ret ";
+        Map m = new HashMap();
+        m.put("ret", false);
+        m.put("bi", bi);
+        return billFeeFacade.findByJpql(j, m);
     }
 
     public BillItem saveBillItem(Bill b, BillEntry e, WebUser wu) {
