2c9f9f254793e8635f949e9ebebb387b741a6d33
PasinduW99 <pwathsara@gmail.com>
2024-02-21 10:20:35 +0530

    Signed-off-by: PasinduW99 <pwathsara@gmail.com>

--- src/main/java/com/divudi/entity/ServiceSession.java
+++ src/main/java/com/divudi/entity/ServiceSession.java
@@ -10,6 +10,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import javax.persistence.Entity;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
@@ -80,7 +81,21 @@ public class ServiceSession extends Item implements Serializable {
     @Transient
     Boolean arival;
     @Transient
-    boolean serviceSessionCreateForOriginatingSession=false;
+    boolean serviceSessionCreateForOriginatingSession = false;
+
+    //new Adittions
+    private int recervedNumbers;
+    private boolean paidAppointmentsOnly;
+    private boolean canChangePatient;
+
+    @Lob
+    private String activities;
+    @Lob
+    private String actions;
+    @Lob
+    private String notificationRoles;
+    @Lob
+    private String dataEntryForms;
 
     public SessionNumberGenerator getSessionNumberGenerator() {
         return sessionNumberGenerator;
@@ -212,7 +227,7 @@ public class ServiceSession extends Item implements Serializable {
     }
 
     public String getDayString() {
-        if (sessionWeekday==null) {
+        if (sessionWeekday == null) {
             return "";
         }
         switch (sessionWeekday) {
@@ -426,5 +441,63 @@ public class ServiceSession extends Item implements Serializable {
     public void setServiceSessionCreateForOriginatingSession(boolean serviceSessionCreateForOriginatingSession) {
         this.serviceSessionCreateForOriginatingSession = serviceSessionCreateForOriginatingSession;
     }
+
+    public int getRecervedNumbers() {
+        return recervedNumbers;
+    }
+
+    public void setRecervedNumbers(int recervedNumbers) {
+        this.recervedNumbers = recervedNumbers;
+    }
+
+    public boolean isPaidAppointmentsOnly() {
+        return paidAppointmentsOnly;
+    }
+
+    public void setPaidAppointmentsOnly(boolean paidAppointmentsOnly) {
+        this.paidAppointmentsOnly = paidAppointmentsOnly;
+    }
+
+    public boolean isCanChangePatient() {
+        return canChangePatient;
+    }
+
+    public void setCanChangePatient(boolean canChangePatient) {
+        this.canChangePatient = canChangePatient;
+    }
+
+    public String getActivities() {
+        return activities;
+    }
+
+    public void setActivities(String activities) {
+        this.activities = activities;
+    }
+
+    public String getActions() {
+        return actions;
+    }
+
+    public void setActions(String actions) {
+        this.actions = actions;
+    }
+
+    public String getNotificationRoles() {
+        return notificationRoles;
+    }
+
+    public void setNotificationRoles(String notificationRoles) {
+        this.notificationRoles = notificationRoles;
+    }
+
+    public String getDataEntryForms() {
+        return dataEntryForms;
+    }
+
+    public void setDataEntryForms(String dataEntryForms) {
+        this.dataEntryForms = dataEntryForms;
+    }
+    
+    
 
 }
