e303d61a5e5c27275df55e33f8af7563cad5c416
Senula88 <senulananayakkara88@gmail.com>
2024-03-04 17:47:15 +0530

    Correct partial payment issue and duplicate bill items Signed-off-by: Senula88 <senulananayakkara88@gmail.com>

--- src/main/java/com/divudi/entity/UserPreference.java
+++ src/main/java/com/divudi/entity/UserPreference.java
@@ -269,6 +269,12 @@ public class UserPreference implements Serializable {
     }
 
     public void setPartialPaymentOfOpdBillsAllowed(boolean partialPaymentOfOpdBillsAllowed) {
+        if(partialPaymentOfOpdBillsAllowed==true){
+            this.opdSettleWithoutCashTendered = true;
+        }
+        else {
+            this.opdSettleWithoutCashTendered = false;
+        }
         this.partialPaymentOfOpdBillsAllowed = partialPaymentOfOpdBillsAllowed;
     }
 
@@ -348,7 +354,6 @@ public class UserPreference implements Serializable {
 
     public void setOpdSettleWithoutCashTendered(boolean opdSettleWithoutCashTendered) {
         this.opdSettleWithoutCashTendered = opdSettleWithoutCashTendered;
-        this.opdSettleWithoutCashTendered = false;
     }
 
     public String getAbbreviationForHistory() {
