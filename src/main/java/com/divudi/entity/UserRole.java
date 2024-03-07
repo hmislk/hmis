a6b38c335a90ea0ce868f335c71c80fa442aec7d
PasinduW99 <pwathsara@gmail.com>
2024-03-07 12:31:10 +0530

    Signed-off-by: PasinduW99 <pwathsara@gmail.com>

---
+++ src/main/java/com/divudi/entity/UserRole.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.divudi.entity;
+
+/**
+ *
+ * @author acer
+ */
+public class UserRole {
+    
+}
