d5b512744665d8b47769413d134c15c6f5cf723f
Thiwanka Madushan <jvptmadushan@gmail.com>
2024-03-08 13:03:04 +0530

    Signed-off-by: Thiwanka Madushan <jvptmadushan@gmail.com>

---
+++ src/main/java/com/divudi/entity/channel/PatientSessionInstanceActivity.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.divudi.entity.channel;
+
+import com.divudi.entity.Patient;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Thiwanka
+ */
+@Entity
+public class PatientSessionInstanceActivity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @OneToMany
+    Patient patient;
+    boolean completed;
+      
+    @ManyToOne
+    AppointmentActivity appointmentActivity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof PatientSessionInstanceActivity)) {
+            return false;
+        }
+        PatientSessionInstanceActivity other = (PatientSessionInstanceActivity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.divudi.entity.channel.PatientSessionInstanceActivity[ id=" + id + " ]";
+    }
+    
+}
