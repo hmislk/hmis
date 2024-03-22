4dff58f3c5c51acace2083de5a17a636bcfdfffc
DamithDeshan <hkddrajapaksha@gmail.com>
2024-03-23 02:01:56 +0530

    closes #4226 Signed-off-by: DamithDeshan <hkddrajapaksha@gmail.com>

--- src/main/java/com/divudi/bean/common/DoctorController.java
+++ src/main/java/com/divudi/bean/common/DoctorController.java
@@ -7,6 +7,7 @@
  * (94) 71 5812399
  */
 package com.divudi.bean.common;
+
 import com.divudi.bean.common.util.JsfUtil;
 import com.divudi.data.Title;
 import com.divudi.entity.Consultant;
@@ -340,9 +341,22 @@ public class DoctorController implements Serializable {
     public void setSpeciality(Speciality speciality) {
         this.speciality = speciality;
     }
-    
-    public Doctor findDoctor(Long id){
+
+    public Doctor findDoctor(Long id) {
         return getFacade().find(id);
+    }
+
+    public Doctor findDoctor(Person person) {
+        String jpql = "select d "
+                + " from Staff d "
+                + " where d.person = :person "
+                + " and d.retired = :ret";
+        
+        Map<String, Object> m = new HashMap<>();
+        m.put("person", person);
+        m.put("ret", false);
+        
+        return getFacade().findFirstByJpql(jpql, m);
     }
 
     /**
