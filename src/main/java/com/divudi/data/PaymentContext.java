742e762d1d5822bef990d4b47080c823ab1a0909
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2024-04-08 03:47:44 +0530

    Payment Methods Improved

---
+++ src/main/java/com/divudi/data/PaymentContext.java
@@ -0,0 +1,18 @@
+package com.divudi.data;
+
+public enum PaymentContext {
+    PURCHASES("Purchases"),
+    ACCEPTING_PAYMENTS("Accepting Payments"),
+    ACCEPTING_PAYMENTS_FOR_CHANNELLING("Accepting Payments for Channelling"),
+    CREDIT_SETTLEMENTS("Credit Settlements");
+
+    private final String label;
+
+    PaymentContext(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
