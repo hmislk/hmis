90646aad228f28c0f076d0d05d7fe9e91019713f
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2024-04-18 05:13:02 +0530

    Closes #4631

--- src/main/java/com/divudi/bean/pharmacy/PharmacySaleController.java
+++ src/main/java/com/divudi/bean/pharmacy/PharmacySaleController.java
@@ -8,6 +8,8 @@ package com.divudi.bean.pharmacy;
 import com.divudi.bean.common.BillBeanController;
 import com.divudi.bean.common.CommonController;
 import com.divudi.bean.common.CommonFunctionsController;
+import com.divudi.bean.common.ConfigOptionApplicationController;
+import com.divudi.bean.common.ConfigOptionController;
 import com.divudi.bean.common.ControllerWithPatient;
 import com.divudi.bean.common.PriceMatrixController;
 import com.divudi.bean.common.SearchController;
@@ -19,6 +21,7 @@ import com.divudi.bean.membership.PaymentSchemeController;
 import com.divudi.data.BillClassType;
 import com.divudi.data.BillNumberSuffix;
 import com.divudi.data.BillType;
+import com.divudi.data.OptionScope;
 import com.divudi.data.PaymentMethod;
 import com.divudi.data.Sex;
 import com.divudi.data.Title;
@@ -107,6 +110,10 @@ public class PharmacySaleController implements Serializable, ControllerWithPatie
     PaymentSchemeController PaymentSchemeController;
     @Inject
     StockController stockController;
+    @Inject
+    ConfigOptionController configOptionController;
+    @Inject
+    ConfigOptionApplicationController configOptionApplicationController;
 
     @Inject
     SessionController sessionController;
@@ -914,7 +921,15 @@ public class PharmacySaleController implements Serializable, ControllerWithPatie
 
     }
 
-    public void addBillItemNew() {
+    public void addBillItem() {
+        if(configOptionApplicationController.getBooleanValueByKey("Add quantity from multiple batches in pharmacy retail billing")){
+            addBillItemMultipleBatches();
+        }else{
+            addBillItemSingleItem();
+        }
+    }
+
+    public void addBillItemSingleItem() {
         editingQty = null;
         errorMessage = null;
 
@@ -992,7 +1007,7 @@ public class PharmacySaleController implements Serializable, ControllerWithPatie
         clearBillItem();
         setActiveIndex(1);
     }
-
+    
     public void addBillItemMultipleBatches() {
         editingQty = null;
         errorMessage = null;
@@ -1029,15 +1044,16 @@ public class PharmacySaleController implements Serializable, ControllerWithPatie
 //            return;
 //        }
 
-        if (checkItemBatch()) {
-            errorMessage = "This batch is already there in the bill.";
-            JsfUtil.addErrorMessage("Already added this item batch");
-            return;
-        }
-        if (!userStockController.isStockAvailable(getStock(), getQty(), getSessionController().getLoggedUser())) {
-            JsfUtil.addErrorMessage("Sorry Already Other User Try to Billing This Stock You Cant Add");
-            return;
-        }
+//        if (checkItemBatch()) {
+//            errorMessage = "This batch is already there in the bill.";
+//            JsfUtil.addErrorMessage("Already added this item batch");
+//            return;
+//        }
+        
+//        if (!userStockController.isStockAvailable(getStock(), getQty(), getSessionController().getLoggedUser())) {
+//            JsfUtil.addErrorMessage("Sorry Already Other User Try to Billing This Stock You Cant Add");
+//            return;
+//        }
 
         calculateAllRatesNew();
         calTotalNew();
@@ -1739,7 +1755,7 @@ public class PharmacySaleController implements Serializable, ControllerWithPatie
         return false;
     }
 
-    public void addBillItem() {
+    public void addBillItemOld() {
         editingQty = null;
 
         if (billItem == null) {
