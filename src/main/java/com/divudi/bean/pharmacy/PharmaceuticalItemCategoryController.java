977d65b0730cf55c5650fb63ce422be390e28fe2
Senula88 <senulananayakkara88@gmail.com>
2024-04-30 23:26:40 +0530

    Closes #4610 Signed-off-by: Senula88 <senulananayakkara88@gmail.com>

--- src/main/java/com/divudi/bean/pharmacy/PharmaceuticalItemCategoryController.java
+++ src/main/java/com/divudi/bean/pharmacy/PharmaceuticalItemCategoryController.java
@@ -136,6 +136,7 @@ public class PharmaceuticalItemCategoryController implements Serializable {
     private boolean errorCheck() {
         if (getCurrent() != null) {
             if (getCurrent().getDescription() == null || getCurrent().getDescription().isEmpty()) {
+                JsfUtil.addSuccessMessage("Please Fill Desciption.");
                 return false;
             } else {
                 String sql;
