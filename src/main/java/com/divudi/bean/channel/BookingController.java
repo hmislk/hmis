b6b89f0a1e459f07ff48d2479b5bdc5495f9ab96
Lawan Samarasekara <lawan.chaamindu1234@gmail.com>
2024-05-26 21:03:51 +0530

    Fixed #5382 Signed-off-by: Lawan Samarasekara <lawan.chaamindu1234@gmail.com>

--- src/main/java/com/divudi/bean/channel/BookingController.java
+++ src/main/java/com/divudi/bean/channel/BookingController.java
@@ -502,7 +502,6 @@ public class BookingController implements Serializable, ControllerWithPatient {
         }
         channelBillController.setPrintPreview(false);
         channelBillController.setPrintPreviewC(false);
-
         channelBillController.setPrintPreviewR(false);
 
         return "/channel/manage_booking?faces-redirect=true";
