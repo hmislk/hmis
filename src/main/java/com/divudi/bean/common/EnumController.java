bc39de801905f30affa658b759b31c96c74984d4
Senula88 <senulananayakkara88@gmail.com>
2024-06-10 18:01:32 +0530

    Signed-off-by: Senula88 <senulananayakkara88@gmail.com>

--- src/main/java/com/divudi/bean/common/EnumController.java
+++ src/main/java/com/divudi/bean/common/EnumController.java
@@ -66,6 +66,7 @@ public class EnumController implements Serializable {
     ConfigOptionApplicationController configOptionApplicationController;
     List<PaymentMethod> paymentMethodsForOpdBilling;
     List<PaymentMethod> paymentMethodsForChanneling;
+    List<PaymentMethod> paymentMethodsForPharmacyBilling;
     SessionNumberType[] sessionNumberTypes;
 
     @PostConstruct
@@ -112,6 +113,23 @@ public class EnumController implements Serializable {
             boolean include = configOptionApplicationController.getBooleanValueByKey(pm.getLabel() + " is available for Channeling", true);
             if (include) {
                 paymentMethodsForChanneling.add(pm);
+            }
+        }
+    }
+    
+    public List<PaymentMethod> getPaymentMethodsForPharmacyBilling() {
+        if (paymentMethodsForPharmacyBilling == null) {
+            fillPaymentMethodsForPharmacyBilling();
+        }
+        return paymentMethodsForPharmacyBilling;
+    }
+    
+    public void fillPaymentMethodsForPharmacyBilling() {
+        paymentMethodsForPharmacyBilling = new ArrayList<>();
+        for (PaymentMethod pm : PaymentMethod.values()) {
+            boolean include = configOptionApplicationController.getBooleanValueByKey(pm.getLabel() + " is available for Pharmacy Billing", true);
+            if (include) {
+                paymentMethodsForPharmacyBilling.add(pm);
             }
         }
     }
