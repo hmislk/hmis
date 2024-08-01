451ac2658098a69f59c6e92951ce30bd94370ada
buddhika75 <buddhika.ari@gmail.com>
2024-08-01 21:41:11 +0530

    Signed-off-by: buddhika75 <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/ws/common/ApplicationConfig.java
+++ src/main/java/com/divudi/ws/common/ApplicationConfig.java
@@ -46,6 +46,8 @@ public class ApplicationConfig extends Application {
         resources.add(com.divudi.ws.inward.ApiInward.class);
         resources.add(com.divudi.ws.lims.Lims.class);
         resources.add(com.divudi.ws.lims.LimsMiddlewareController.class);
+        resources.add(com.divudi.ws.lims.MiddlewareController.class);
+        
     }
     
 }
