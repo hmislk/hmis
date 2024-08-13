f0eb89d0eb122c5a17c816ff530f6775e7891c74
thiwanka570 <jvptmadushan@gmail.com>
2024-08-13 10:26:07 +0530

    Signed-off-by: thiwanka570 <jvptmadushan@gmail.com>

--- src/main/java/com/divudi/bean/hr/StaffController.java
+++ src/main/java/com/divudi/bean/hr/StaffController.java
@@ -1200,6 +1200,7 @@ public class StaffController implements Serializable {
         updateStaffEmployment();
 
         recreateModel();
+        getItems();
         fillSelectedItemsWithAllStaff();
     }
 
