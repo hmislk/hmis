b0f62828b253d77b3c4e0764604d23a72ea4222c
Buddhika <buddhika.ari@gmail.com>
2024-08-18 17:12:13 +0530

    Signed-off-by: Buddhika <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/ws/common/ApplicationConfig.java
+++ src/main/java/com/divudi/ws/common/ApplicationConfig.java
@@ -36,6 +36,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.divudi.ws.channel.ChannelApi.class);
         resources.add(com.divudi.ws.common.ApiMembership.class);
         resources.add(com.divudi.ws.common.ConfigResource.class);
         resources.add(com.divudi.ws.fhir.Fhir.class);
