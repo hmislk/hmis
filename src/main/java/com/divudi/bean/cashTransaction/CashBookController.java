44f86d87276913b5c1f845b7a4db142d89e8ed28
Buddhika <buddhika.ari@gmail.com>
2024-08-19 22:21:28 +0530

    Added CashBookConverter to the Controller Signed-off-by: Buddhika <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/bean/cashTransaction/CashBookController.java
+++ src/main/java/com/divudi/bean/cashTransaction/CashBookController.java
@@ -11,6 +11,10 @@ import com.divudi.facade.CashBookFacade;
 import java.io.Serializable;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -48,6 +52,50 @@ public class CashBookController implements Serializable {
 
     public void setCashBook(CashBook cashBook) {
         this.cashBook = cashBook;
+    }
+    
+    
+    /**
+     *
+     */
+    @FacesConverter(forClass = CashBook.class)
+    public static class CashBookConverter implements Converter {
+
+        @Override
+        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+            if (value == null || value.length() == 0) {
+                return null;
+            }
+            CashBookController controller = (CashBookController) facesContext.getApplication().getELResolver().
+                    getValue(facesContext.getELContext(), null, "CashBookController");
+            return controller.getCashbookFacade().find(getKey(value));
+        }
+
+        java.lang.Long getKey(String value) {
+            java.lang.Long key;
+            key = Long.valueOf(value);
+            return key;
+        }
+
+        String getStringKey(java.lang.Long value) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(value);
+            return sb.toString();
+        }
+
+        @Override
+        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+            if (object == null) {
+                return null;
+            }
+            if (object instanceof CashBook) {
+                CashBook o = (CashBook) object;
+                return getStringKey(o.getId());
+            } else {
+                throw new IllegalArgumentException("object " + object + " is of type "
+                        + object.getClass().getName() + "; expected type: " + CashBook.class.getName());
+            }
+        }
     }
 
 }
