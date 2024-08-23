e03f8ec98c9f88a6644a645988b54501970d8e1b
thiwanka570 <jvptmadushan@gmail.com>
2024-08-23 14:05:33 +0530

    Signed-off-by: thiwanka570 <jvptmadushan@gmail.com>

--- src/main/java/com/divudi/bean/cashTransaction/CashBookEntryController.java
+++ src/main/java/com/divudi/bean/cashTransaction/CashBookEntryController.java
@@ -6,11 +6,19 @@
 package com.divudi.bean.cashTransaction;
 
 import com.divudi.bean.common.SessionController;
+import com.divudi.bean.common.util.JsfUtil;
+import com.divudi.data.PaymentMethod;
+import static com.divudi.data.PaymentMethod.Card;
+import static com.divudi.data.PaymentMethod.OnCall;
+import static com.divudi.data.PaymentMethod.PatientDeposit;
+import com.divudi.entity.Payment;
 import com.divudi.entity.cashTransaction.CashBook;
 import com.divudi.entity.cashTransaction.CashBookEntry;
 import com.divudi.facade.CashBookEntryFacade;
 import com.divudi.facade.CashBookFacade;
+import com.divudi.facade.PaymentFacade;
 import java.io.Serializable;
+import java.util.Date;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.component.UIComponent;
@@ -29,23 +37,111 @@ import javax.inject.Named;
 public class CashBookEntryController implements Serializable {
 
     @EJB
-    private CashBookEntryFacade CashbookEntryFacade;
+    private CashBookEntryFacade cashbookEntryFacade;
     @EJB
+    PaymentFacade paymentFacade;
+
     private CashBook cashBook;
-    
     @Inject
     private SessionController sessionController;
 
-    
+    CashBookEntry current;
+
+    public void writeCashBookEntry(Payment p) {
+        if (p == null) {
+            JsfUtil.addErrorMessage("Cashbook Entry Error !");
+            return;
+        }
+
+        if (!chackPaymentMethodForCashBookEntry(p.getPaymentMethod())) {
+            return;
+        }
+
+        current = new CashBookEntry();
+        current.setInstitution(p.getInstitution());
+        current.setDepartment(p.getDepartment());
+        current.setCreater(sessionController.getLoggedUser());
+        current.setCreatedAt(new Date());
+        current.setPaymentMethod(p.getPaymentMethod());
+        current.setEntryValue(p.getPaidValue());
+        current.setPayment(p);
+        current.setCashBook(sessionController.getLoggedCashbook());
+        cashbookEntryFacade.create(current);
+
+    }
+
+    public boolean chackPaymentMethodForCashBookEntry(PaymentMethod pm) {
+        boolean check = false;
+        if (pm == null) {
+            JsfUtil.addErrorMessage("Payment method is not found !");
+            return false;
+        }
+        switch (pm) {
+            case Card:
+                check = true;
+                break;
+
+            case Cash:
+                check = false;
+                break;
+
+            case Cheque:
+                check = false;
+                break;
+
+            case Agent:
+                check = false;
+                break;
+
+            case Credit:
+                check = false;
+                break;
+
+            case OnCall:
+                check = false;
+                break;
+
+            case PatientDeposit:
+                check = true;
+                break;
+
+            case Slip:
+                check = true;
+                break;
+
+            case Staff:
+                check = false;
+                break;
+
+            case Staff_Welfare:
+                check = false;
+                break;
+
+            case ewallet:
+                check = true;
+                break;
+
+            case OnlineSettlement:
+                check = true;
+                break;
+
+            default:
+
+        }
+
+        return check;
+
+    }
+
     public CashBookEntryController() {
     }
 
     public CashBookEntryFacade getCashbookEntryFacade() {
-        return CashbookEntryFacade;
+        return cashbookEntryFacade;
     }
 
     public void setCashbookEntryFacade(CashBookEntryFacade CashbookEntryFacade) {
-        this.CashbookEntryFacade = CashbookEntryFacade;
+        this.cashbookEntryFacade = CashbookEntryFacade;
     }
 
     public CashBook getCashBook() {
@@ -55,8 +151,7 @@ public class CashBookEntryController implements Serializable {
     public void setCashBook(CashBook cashBook) {
         this.cashBook = cashBook;
     }
-    
-    
+
     /**
      *
      */
