1cd8d26af97e4ef2c78dac9bc6585a1e85f30644
Buddhika Ariyaratne <buddhika.ari@gmail.com>
2024-08-26 02:13:49 +0530

    Closes #6980 Signed-off-by: Buddhika Ariyaratne <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/bean/opd/OpdBillController.java
+++ src/main/java/com/divudi/bean/opd/OpdBillController.java
@@ -3124,7 +3124,7 @@ public class OpdBillController implements Serializable, ControllerWithPatient, C
     public List<ItemLight> completeOpdItemsByWord(String query) {
         List<ItemLight> filteredItems = new ArrayList<>();
         Long defaultValue = 10l;
-        Long maxResultsLong = configOptionApplicationController.getLongValueByKey("OPD Bill Fees are the same for all departments, institutions and sites.",  defaultValue);
+        Long maxResultsLong = configOptionApplicationController.getLongValueByKey("Number of Maximum Results for Item Search in Autocompletes",  defaultValue);
         int maxResults = maxResultsLong.intValue();
         
         boolean addAllBillFees = configOptionApplicationController.getBooleanValueByKey("OPD Bill Fees are the same for all departments, institutions and sites.", true);
