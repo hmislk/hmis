3e6ca6e7283ece527bcbaf634f47c62a74af2ed5
DamithDeshan <hkddrajapaksha@gmail.com>
2024-08-29 13:57:40 +0530

    Signed-off-by: DamithDeshan <hkddrajapaksha@gmail.com>

--- src/main/java/com/divudi/bean/common/NavigationController.java
+++ src/main/java/com/divudi/bean/common/NavigationController.java
@@ -154,7 +154,7 @@ public class NavigationController implements Serializable {
     }
     
     public String navigateToPaymentSchemeDiscountOpdBySite() {
-        return "/admin/pricing/payment_scheme_discount_opd_by_department?faces-redirect=true";
+        return "/admin/pricing/payment_scheme_discount_opd_by_site?faces-redirect=true";
     }
 
     public String navigateToPaymentSchemeDiscountOpdByItem() {
