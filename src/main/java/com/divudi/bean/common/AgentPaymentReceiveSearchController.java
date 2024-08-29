2182f50b67a6bfcbff85324e90733d073140a187
Lawan Samarasekara <lawan.chaamindu1234@gmail.com>
2024-08-29 14:56:53 +0530

    Fixed #7014 Signed-off-by: Lawan Samarasekara <lawan.chaamindu1234@gmail.com>

--- src/main/java/com/divudi/bean/common/AgentPaymentReceiveSearchController.java
+++ src/main/java/com/divudi/bean/common/AgentPaymentReceiveSearchController.java
@@ -19,6 +19,7 @@ import com.divudi.entity.BillFee;
 import com.divudi.entity.BillItem;
 import com.divudi.entity.BilledBill;
 import com.divudi.entity.CancelledBill;
+import com.divudi.entity.Payment;
 import com.divudi.entity.WebUser;
 import com.divudi.facade.BillComponentFacade;
 import com.divudi.facade.BillFacade;
@@ -27,6 +28,7 @@ import com.divudi.facade.BillItemFacade;
 import com.divudi.facade.BilledBillFacade;
 import com.divudi.facade.CancelledBillFacade;
 import com.divudi.facade.InstitutionFacade;
+import com.divudi.facade.PaymentFacade;
 import com.divudi.facade.RefundBillFacade;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -78,6 +80,9 @@ public class AgentPaymentReceiveSearchController implements Serializable {
     AgentPaymentRecieveBillController agentPaymentRecieveBillController;
     @EJB
     EjbApplication ejbApplication;
+    @EJB
+    PaymentFacade paymentFacade;
+    
     private List<BillItem> tempbillItems;
     private String comment;
     WebUser user;
@@ -287,6 +292,9 @@ public class AgentPaymentReceiveSearchController implements Serializable {
                 getBill().setCancelledBill(cb);
                 getBilledBillFacade().edit(getBill());
                 JsfUtil.addSuccessMessage("Cancelled");
+                createPayment(cb, paymentMethod);
+                
+                System.out.println("cb.getNetTotal() = " + cb.getNetTotal());
 
                 //for channel agencyHistory Update
                 getAgentPaymentRecieveBillController().createAgentHistory(cb.getFromInstitution(), cb.getNetTotal(), historyType, cb);
@@ -335,6 +343,29 @@ public class AgentPaymentReceiveSearchController implements Serializable {
 //
 //        billForCancel = null;
 //    }
+    
+    public Payment createPayment(Bill bill, PaymentMethod pm) {
+        Payment p = new Payment();
+        p.setBill(bill);
+        setPaymentMethodData(p, pm);
+        return p;
+    }
+
+    public void setPaymentMethodData(Payment p, PaymentMethod pm) {
+
+        p.setInstitution(getSessionController().getInstitution());
+        p.setDepartment(getSessionController().getDepartment());
+        p.setCreatedAt(new Date());
+        p.setCreater(getSessionController().getLoggedUser());
+        p.setPaymentMethod(pm);
+
+        p.setPaidValue(p.getBill().getNetTotal());
+
+        if (p.getId() == null) {
+            paymentFacade.create(p);
+        }
+
+    }
 
     public List<Bill> getBillsToApproveCancellation() {
         //////// // System.out.println("1");
