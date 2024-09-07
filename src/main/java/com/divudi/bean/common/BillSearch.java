987d2340d19f5d635ff603630f8f3b94f7d60e11
Piyankara N W M G P <pubudupiyankara.me@gmail.com>
2024-09-08 00:02:06 +0530

    Signed-off-by: Piyankara N W M G P <pubudupiyankara.me@gmail.com>

--- src/main/java/com/divudi/bean/common/BillSearch.java
+++ src/main/java/com/divudi/bean/common/BillSearch.java
@@ -3025,6 +3025,7 @@ public class BillSearch implements Serializable {
         }
         BillTypeAtomic billTypeAtomic = bill.getBillTypeAtomic();
         loadBillDetails(bill);
+        System.out.println("billTypeAtomic = " + billTypeAtomic);
         switch (billTypeAtomic) {
             case PHARMACY_RETAIL_SALE_CANCELLED:
                 pharmacyBillSearch.setBill(bill);
@@ -3083,6 +3084,7 @@ public class BillSearch implements Serializable {
                 return navigateToViewCcPaymentMadeCancellationBill(bill);
 
             case CC_PAYMENT_RECEIVED_BILL:
+                System.out.println("CC_PAYMENT_RECEIVED_BILL");
                 return navigateToViewCcPaymentReceivedBill(bill);
 
             case CHANNEL_REFUND:
@@ -3151,7 +3153,9 @@ public class BillSearch implements Serializable {
     }
 
     public String navigateToViewCcPaymentReceivedBill(Bill bill) {
+        System.out.println("navigateToViewCcPaymentReceivedBill");
         loadBillDetails(bill);
+        System.out.println("bill = " + bill);
         return "/collecting_centre/view/cc_payment_received_bill_view";
     }
 
