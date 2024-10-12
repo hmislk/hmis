3c4a3dab79758902a3eecae06f8e3b72d4925943
Buddhika <buddhika.ari@gmail.com>
2024-10-12 21:48:26 +0530

    Signed-off-by: Buddhika <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/bean/common/BillReturnController.java
+++ src/main/java/com/divudi/bean/common/BillReturnController.java
@@ -1,7 +1,9 @@
 package com.divudi.bean.common;
 
+import com.divudi.bean.cashTransaction.PaymentController;
 import com.divudi.bean.common.util.JsfUtil;
 import com.divudi.data.BillTypeAtomic;
+import com.divudi.data.PaymentMethod;
 import com.divudi.ejb.BillNumberGenerator;
 import com.divudi.entity.Bill;
 import com.divudi.entity.BillFee;
@@ -12,6 +14,7 @@ import com.divudi.facade.BillFacade;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Inject;
@@ -31,9 +34,17 @@ public class BillReturnController implements Serializable {
     BillNumberGenerator billNumberGenerator;
 
     @Inject
+    SessionController sessionController;
+    @Inject
     BillController billController;
     @Inject
     BillBeanController billBeanController;
+    @Inject
+    BillItemController billItemController;
+    @Inject
+    BillFeeController billFeeController;
+    @Inject
+    PaymentController paymentController;
 
     private Bill originalBillToReturn;
     private List<BillItem> originalBillItemsAvailableToReturn;
@@ -42,7 +53,9 @@ public class BillReturnController implements Serializable {
     private Bill newlyReturnedBill;
     private List<BillItem> newlyReturnedBillItems;
     private List<BillFee> newlyReturnedBillFees;
-    private List<Payment> returningBillPayment;
+    private List<Payment> returningBillPayments;
+
+    private PaymentMethod paymentMethod;
 
     private boolean returningStarted = false;
 
@@ -57,38 +70,119 @@ public class BillReturnController implements Serializable {
             return null;
         }
         originalBillItemsAvailableToReturn = billBeanController.fetchBillItems(originalBillToReturn);
+        returningStarted=false;
         return "/opd/bill_return?faces-redirect=true";
     }
 
-    public void settleOpdReturnBill() {
+    public String settleOpdReturnBill() {
         if (returningStarted) {
             JsfUtil.addErrorMessage("Already Returning Started");
-            return;
+            return null;
         }
         if (originalBillToReturn == null) {
             JsfUtil.addErrorMessage("Already Returning Started");
-            return;
+            returningStarted = false;
+            return null;
         }
-        if(originalBillItemsToSelectedToReturn==null || originalBillItemsToSelectedToReturn.isEmpty()){
+        if (originalBillItemsToSelectedToReturn == null || originalBillItemsToSelectedToReturn.isEmpty()) {
             JsfUtil.addErrorMessage("Nothing selected to return");
-            return;
+            returningStarted = false;
+            return null;
         }
         originalBillToReturn = billFacade.findWithoutCache(originalBillToReturn.getId());
-        if(originalBillToReturn.isCancelled()){
+        if (originalBillToReturn.isCancelled()) {
             JsfUtil.addErrorMessage("Already Cancelled");
-            return;
+            returningStarted = false;
+            return null;
         }
-        if(originalBillToReturn.isRefunded()){
+        //TO DO: Check weather selected items is refunded
+        if (originalBillToReturn.isRefunded()) {
             JsfUtil.addErrorMessage("Already Cancelled");
-            return;
+            returningStarted = false;
+            return null;
         }
         // fetch original bill now, checked alteady returned, cancelled, drawer balance, 
         newlyReturnedBill = new RefundBill();
         newlyReturnedBill.copy(originalBillToReturn);
         newlyReturnedBill.setBillTypeAtomic(BillTypeAtomic.OPD_BILL_REFUND);
         newlyReturnedBill.invertValue();
+        String deptId = billNumberGenerator.departmentBillNumberGeneratorYearly(sessionController.getDepartment(),
+                BillTypeAtomic.OPD_BILL_REFUND);
+        newlyReturnedBill.setDeptId(deptId);
+        newlyReturnedBill.setInsId(deptId);
         billController.save(newlyReturnedBill);
+
+        double returningTotal = 0.0;
+        double returningNetTotal = 0.0;
+        double returningHospitalTotal = 0.0;
+        double returningStaffTotal = 0.0;
+        double returningDiscount = 0.0;
+
+        newlyReturnedBillItems = new ArrayList<>();
+        returningBillPayments=new ArrayList<>();
+        newlyReturnedBillFees=new ArrayList<>();
         
+        for (BillItem selectedBillItemToReturn : originalBillItemsToSelectedToReturn) {
+
+            returningTotal += selectedBillItemToReturn.getGrossValue();
+            returningNetTotal += selectedBillItemToReturn.getNetValue();
+            returningHospitalTotal += selectedBillItemToReturn.getHospitalFee();
+            returningStaffTotal += selectedBillItemToReturn.getStaffFee();
+            returningDiscount += selectedBillItemToReturn.getDiscount();
+
+            BillItem newlyCreatedReturningItem = new BillItem();
+            newlyCreatedReturningItem.copy(selectedBillItemToReturn);
+            newlyCreatedReturningItem.invertValue();
+            newlyCreatedReturningItem.setBill(newlyReturnedBill);
+            newlyCreatedReturningItem.setReferanceBillItem(selectedBillItemToReturn);
+            billItemController.save(newlyCreatedReturningItem);
+            newlyReturnedBillItems.add(newlyCreatedReturningItem);
+            selectedBillItemToReturn.setRefunded(true);
+            selectedBillItemToReturn.setReferanceBillItem(newlyCreatedReturningItem);
+            billItemController.save(selectedBillItemToReturn);
+            List<BillFee> originalBillFeesOfSelectedBillItem = billBeanController.fetchBillFees(selectedBillItemToReturn);
+            
+            
+            
+            if (originalBillFeesOfSelectedBillItem != null) {
+                for (BillFee origianlFee : originalBillFeesOfSelectedBillItem) {
+                    BillFee newlyCreatedBillFeeToReturn = new BillFee();
+                    newlyCreatedBillFeeToReturn.copy(origianlFee);
+                    newlyCreatedBillFeeToReturn.invertValue();
+                    newlyCreatedBillFeeToReturn.setBill(newlyReturnedBill);
+                    newlyCreatedBillFeeToReturn.setBillItem(newlyCreatedReturningItem);
+                    newlyCreatedBillFeeToReturn.setReferenceBillFee(origianlFee);
+                    newlyCreatedBillFeeToReturn.setReferenceBillItem(selectedBillItemToReturn);
+                    billFeeController.save(newlyCreatedBillFeeToReturn);
+                    newlyReturnedBillFees.add(newlyCreatedBillFeeToReturn);
+
+                    origianlFee.setReturned(true);
+                    origianlFee.setReferenceBillFee(newlyCreatedBillFeeToReturn);
+                    origianlFee.setReferenceBillItem(newlyCreatedReturningItem);
+                    billFeeController.save(origianlFee);
+
+                }
+            }
+        }
+
+        newlyReturnedBill.setGrantTotal(0 - returningTotal);
+        newlyReturnedBill.setNetTotal(0 - returningNetTotal);
+        newlyReturnedBill.setTotal(0 - returningTotal);
+        newlyReturnedBill.setHospitalFee(0 - returningHospitalTotal);
+        newlyReturnedBill.setProfessionalFee(0 - returningStaffTotal);
+        newlyReturnedBill.setDiscount(0 - returningDiscount);
+        billController.save(newlyReturnedBill);
+
+        Payment returningPayment = new Payment();
+        returningPayment.setBill(newlyReturnedBill);
+        returningPayment.setPaymentMethod(paymentMethod);
+        returningPayment.setPaidValue(newlyReturnedBill.getNetTotal());
+        paymentController.save(returningPayment);
+        returningBillPayments.add(returningPayment);
+
+        returningStarted = false;
+        return "/opd/bill_return_print?faces-redirect=true";
+
     }
 
     public Bill getOriginalBillToReturn() {
@@ -139,12 +233,12 @@ public class BillReturnController implements Serializable {
         this.newlyReturnedBillFees = newlyReturnedBillFees;
     }
 
-    public List<Payment> getReturningBillPayment() {
-        return returningBillPayment;
+    public List<Payment> getReturningBillPayments() {
+        return returningBillPayments;
     }
 
-    public void setReturningBillPayment(List<Payment> returningBillPayment) {
-        this.returningBillPayment = returningBillPayment;
+    public void setReturningBillPayments(List<Payment> returningBillPayments) {
+        this.returningBillPayments = returningBillPayments;
     }
 
     public boolean isReturningStarted() {
@@ -154,7 +248,13 @@ public class BillReturnController implements Serializable {
     public void setReturningStarted(boolean returningStarted) {
         this.returningStarted = returningStarted;
     }
-    
-    
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
 
 }
