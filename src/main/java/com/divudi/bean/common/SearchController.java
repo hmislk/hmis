f8f8f8122fd10f55454e1829cac5ab13aa1ef3cb
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2024-11-29 04:15:39 +0530

    Signed-off-by: Dr M H B Ariyaratne <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/bean/common/SearchController.java
+++ src/main/java/com/divudi/bean/common/SearchController.java
@@ -833,9 +833,9 @@ public class SearchController implements Serializable {
         return "/analytics/professional_payment_list?faces-redirect=true";
     }
     
-    public String navigateToWhtReport(){
-        return "/reports/financialReports/wht?faces-redirect=true";
-    }
+//    public String navigateToWhtReport(){
+//        return "/reports/financialReports/wht?faces-redirect=true";
+//    }
 
     public String toSearchBills() {
         bills = null;
