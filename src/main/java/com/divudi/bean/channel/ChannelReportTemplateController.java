6608327bd0a13cce8fcce457e8fa5d4193b94335
Chinthaka Prasad <chinthakaprasad30@gmail.com>
2024-12-04 01:10:50 +0530

    fact: online booking report is done. Signed-off-by: Chinthaka Prasad <chinthakaprasad30@gmail.com>

--- src/main/java/com/divudi/bean/channel/ChannelReportTemplateController.java
+++ src/main/java/com/divudi/bean/channel/ChannelReportTemplateController.java
@@ -901,6 +901,7 @@ public class ChannelReportTemplateController implements Serializable {
                 + " where bs.retired=false "
                 + " and bs.bill.creditCompany is not null "
                 + " and bs.bill.creditCompany.name = 'DOC_990'"
+                + " and bs.bill.billTypeAtomic = :bta"
                 + " and bs.sessionInstance.sessionDate between :fd and :td ";
 
         if (institution != null) {
@@ -917,6 +918,7 @@ public class ChannelReportTemplateController implements Serializable {
 
         m.put("fd", fromDate);
         m.put("td", toDate);
+        m.put("bta", BillTypeAtomic.CHANNEL_BOOKING_FOR_PAYMENT_ONLINE_COMPLETED_PAYMENT);
 
         List<ReportTemplateRow> rs = (List<ReportTemplateRow>) billFacade.findLightsByJpql(j, m, TemporalType.DATE);
 
