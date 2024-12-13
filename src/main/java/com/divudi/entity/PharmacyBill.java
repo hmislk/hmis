b3dde227c2af529cfca027f6089dfb72ab1f5dd4
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2024-12-13 12:37:02 +0530

    Closes #9596 Quantity_not_recorded_in_stockHistory Signed-off-by: Dr M H B Ariyaratne <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/entity/PharmacyBill.java
+++ src/main/java/com/divudi/entity/PharmacyBill.java
@@ -1,16 +1,12 @@
 package com.divudi.entity;
 
 import java.io.Serializable;
-import java.util.Date;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
-import javax.persistence.Temporal;
 
 /**
  *
