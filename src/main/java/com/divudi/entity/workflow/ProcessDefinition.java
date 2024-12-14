462d471a9ea2e3c97c620089ee0677597bc41e8f
Buddhika Ariyaratne <buddhika.ari@gmail.com>
2024-12-14 17:47:58 +0530

    Signed-off-by: Buddhika Ariyaratne <buddhika.ari@gmail.com>

---
+++ src/main/java/com/divudi/entity/workflow/ProcessDefinition.java
@@ -0,0 +1,77 @@
+package com.divudi.entity.workflow;
+
+import com.divudi.entity.WebUser;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Dr M H B Ariyaratne
+ *
+ */
+@Entity
+public class ProcessDefinition implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    String name;
+    String description;
+    String version;
+    boolean active;
+    ProcessDefinition parent;
+
+    //Creater Properties
+    @ManyToOne
+    private WebUser creater;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date createdAt;
+    //Retairing properties
+    private boolean retired;
+    @ManyToOne
+    private WebUser retirer;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date retiredAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ProcessDefinition)) {
+            return false;
+        }
+        ProcessDefinition other = (ProcessDefinition) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.divudi.entity.workflow.ProcessDefinition[ id=" + id + " ]";
+    }
+
+}
