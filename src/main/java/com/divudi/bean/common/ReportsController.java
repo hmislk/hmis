dd66b2cd35a95b03ee6e950a21f1feb67d62c302
imexh <chandikaranawella@gmail.com>
2024-12-19 10:30:40 +0530

    added charts to route analysis report

--- src/main/java/com/divudi/bean/common/ReportsController.java
+++ src/main/java/com/divudi/bean/common/ReportsController.java
@@ -30,6 +30,7 @@ import com.divudi.light.common.BillSummaryRow;
 import com.divudi.service.BillService;
 import com.divudi.service.PatientInvestigationService;
 import org.primefaces.model.StreamedContent;
+import org.primefaces.model.charts.line.LineChartModel;
 import org.primefaces.model.file.UploadedFile;
 
 import javax.ejb.EJB;
@@ -309,6 +310,8 @@ public class ReportsController implements Serializable {
     private String settlementStatus;
     private String dischargedStatus;
 
+    private boolean showChart;
+
     public String getDischargedStatus() {
         return dischargedStatus;
     }
@@ -322,11 +325,11 @@ public class ReportsController implements Serializable {
         return paymentMethod;
     }
 
-    
+
     public void setPaymentMethod(PaymentMethod paymentMethod) {
         this.paymentMethod = paymentMethod;
     }
-    
+
 
     public CommonController getCommonController() {
         return commonController;
@@ -1518,6 +1521,14 @@ public class ReportsController implements Serializable {
         this.groupedRouteWiseBillsMonthly = groupedRouteWiseBillsMonthly;
     }
 
+    public boolean isShowChart() {
+        return showChart;
+    }
+
+    public void setShowChart(boolean showChart) {
+        this.showChart = showChart;
+    }
+
     public void generateSampleCarrierReport() {
         System.out.println("generateSampleCarrierReport = " + this);
         bundle = new ReportTemplateRowBundle();
@@ -2197,6 +2208,38 @@ public class ReportsController implements Serializable {
         return total;
     }
 
+    public Map<YearMonth, Double> getSampleCountChartData() {
+        Map<YearMonth, Double> data = new HashMap<>();
+
+        if (reportType.equalsIgnoreCase("detail")) {
+            for (YearMonth yearMonth : yearMonths) {
+                data.put(yearMonth, getCollectionCenterWiseTotalSampleCount(yearMonth) / calculateCollectionCenterWiseBillCount(yearMonth));
+            }
+        } else {
+            for (YearMonth yearMonth : yearMonths) {
+                data.put(yearMonth, calculateRouteWiseTotalSampleCount(yearMonth) / calculateRouteWiseBillCount(yearMonth));
+            }
+        }
+
+        return data;
+    }
+
+    public Map<YearMonth, Double> getServiceAmountChartData() {
+        Map<YearMonth, Double> data = new HashMap<>();
+
+        if (reportType.equalsIgnoreCase("detail")) {
+            for (YearMonth yearMonth : yearMonths) {
+                data.put(yearMonth, getCollectionCenterWiseTotalServiceAmount(yearMonth) / calculateCollectionCenterWiseBillCount(yearMonth));
+            }
+        } else {
+            for (YearMonth yearMonth : yearMonths) {
+                data.put(yearMonth, calculateRouteWiseTotalServiceAmount(yearMonth) / calculateRouteWiseBillCount(yearMonth));
+            }
+        }
+
+        return data;
+    }
+
     public ReportTemplateRowBundle generateCollectingCenterWiseBillItems(List<BillTypeAtomic> bts) {
         Map<String, Object> parameters = new HashMap<>();
         String jpql = "SELECT new com.divudi.data.ReportTemplateRow(bill) "
@@ -2373,7 +2416,7 @@ public class ReportsController implements Serializable {
     }
 
     public ReportTemplateRowBundle generateDebtorBalanceReportBills(List<BillTypeAtomic> bts, List<PaymentMethod> billPaymentMethods,
-            boolean onlyDueBills) {
+                                                                    boolean onlyDueBills) {
         Map<String, Object> parameters = new HashMap<>();
         String jpql = "SELECT new com.divudi.data.ReportTemplateRow(bill) "
                 + "FROM Bill bill "
@@ -3245,7 +3288,7 @@ public class ReportsController implements Serializable {
 
         bundle.setGroupedBillItemsByInstitution(billMap);
     }
-    
+
     public Double calculateNetTotalByBills(List<Bill> bills) {
         Double netTotal = 0.0;
 
@@ -3266,7 +3309,7 @@ public class ReportsController implements Serializable {
         return discount;
     }
 
-    
+
     public Double calculateSubTotal() {
         double subTotal = 0.0;
         Map<Institution, List<Bill>> billMap = bundle.getGroupedBillItemsByInstitution();
