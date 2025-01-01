777d2c091621b7442af3ad33a0fa4eb28c03f3ea
Chinthaka Prasad <chinthakaprasad30@gmail.com>
2025-01-01 14:31:28 +0530

    Signed-off-by: Chinthaka Prasad <chinthakaprasad30@gmail.com>

--- src/main/java/com/divudi/ejb/PharmacyCalculation.java
+++ src/main/java/com/divudi/ejb/PharmacyCalculation.java
@@ -882,6 +882,8 @@ public class PharmacyCalculation implements Serializable {
     }
 
     public void calculateRetailSaleValueAndFreeValueAtPurchaseRate(Bill b) {
+        
+        
         double sale = 0.0;
         double free = 0.0;
 
