1c5dd56f303dab2704f60d0be04bd1c0bd846964
Dr. M H B Ariyaratne <buddhika.ari@gmail.com>
2025-02-10 10:03:16 +0530

    Closes #10462 (#10468)
    Signed-off-by: Dr M H B Ariyaratne <buddhika.ari@gmail.com>
    
    Co-authored-by: imexh <chandikaranawella@gmail.com>

--- src/main/webapp/resources/sql/bills.sql
+++ src/main/webapp/resources/sql/bills.sql
@@ -1,4 +1,5 @@
 select `ID`, 
+`CREATEDAT`,
 `COMPLETED`,
 `BILLEDBILL_ID`,
 `REFERENCEBILL_ID`,
@@ -8,7 +9,6 @@ select `ID`,
 `BILLTYPEATOMIC`, 
 `REFERENCENUMBER`,
 `PAIDAMOUNT`, 
-`SETTLEDAMOUNTBYPATIENT`, `SETTLEDAMOUNTBYSPONSOR`, `PAIDAT`,
 `DEPTID`, 
 `COMMENTS`,
  `CREATEDAT`,
@@ -28,4 +28,4 @@ select `ID`,
 from bill 
 where id in(708428, 669850)
 order by `ID` desc limit 10;
-select * from billnumber limit 100;
+
