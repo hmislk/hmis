881bb7c766940547092d76a820cec3ef430572e1
imexh <chandikaranawella@gmail.com>
2025-03-27 10:03:26 +0530

    Added monitoring to sample carrier report

---
+++ src/main/java/com/divudi/data/reports/LaboratoryReport.java
@@ -0,0 +1,26 @@
+package com.divudi.data.reports;
+
+public enum LaboratoryReport implements IReportType {
+    SAMPLE_CARRIER_REPORT("Sample Carrier Report");
+
+    private final String displayName;
+
+    LaboratoryReport(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String getReportType() {
+        return this.getClass().getSimpleName();
+    }
+
+    @Override
+    public String getValue() {
+        return this.name();
+    }
+}
