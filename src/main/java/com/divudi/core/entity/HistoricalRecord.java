d560adcc7718132d4cd9cced6dc682836bea746f
Dr M H Buddhika Ariyaratne <buddhika.ari@gmail.com>
2025-04-26 15:17:17 +0530

    Signed-off-by: Dr M H Buddhika Ariyaratne <buddhika.ari@gmail.com>

---
+++ src/main/java/com/divudi/core/entity/HistoricalRecord.java
@@ -0,0 +1,178 @@
+package com.divudi.core.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Buddhika & OpenAI ChatGPT
+ */
+@Entity
+public class HistoricalRecord implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String variableName;
+
+    private Double value;
+
+    @ManyToOne
+    private Institution institution;
+
+    @ManyToOne
+    private Institution site;
+
+    @ManyToOne
+    private Department department;
+
+    @Temporal(TemporalType.DATE)
+    private Date recordDate;
+
+    private Boolean retired = false;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @ManyToOne
+    private WebUser createdBy;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date retiredAt;
+
+    @ManyToOne
+    private WebUser retiredBy;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof HistoricalRecord)) {
+            return false;
+        }
+        HistoricalRecord other = (HistoricalRecord) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.divudi.core.entity.HistoricalRecord[ id=" + id + " ]";
+    }
+
+    public String getVariableName() {
+        return variableName;
+    }
+
+    public void setVariableName(String variableName) {
+        this.variableName = variableName;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Institution getInstitution() {
+        return institution;
+    }
+
+    public void setInstitution(Institution institution) {
+        this.institution = institution;
+    }
+
+    public Institution getSite() {
+        return site;
+    }
+
+    public void setSite(Institution site) {
+        this.site = site;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Date getRecordDate() {
+        return recordDate;
+    }
+
+    public void setRecordDate(Date recordDate) {
+        this.recordDate = recordDate;
+    }
+
+    public Boolean getRetired() {
+        return retired;
+    }
+
+    public void setRetired(Boolean retired) {
+        this.retired = retired;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public WebUser getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(WebUser createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getRetiredAt() {
+        return retiredAt;
+    }
+
+    public void setRetiredAt(Date retiredAt) {
+        this.retiredAt = retiredAt;
+    }
+
+    public WebUser getRetiredBy() {
+        return retiredBy;
+    }
+
+    public void setRetiredBy(WebUser retiredBy) {
+        this.retiredBy = retiredBy;
+    }
+    
+}
---
+++ src/main/java/com/divudi/service/HistoricalRecordService.java
@@ -0,0 +1,70 @@
+/*
+ * Created by ChatGPT on request by Dr. M. H. B. Ariyaratne
+ */
+package com.divudi.service;
+
+import com.divudi.core.entity.Department;
+import com.divudi.core.entity.HistoricalRecord;
+import com.divudi.core.entity.Institution;
+import com.divudi.core.facade.HistoricalRecordFacade;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author ASUS
+ */
+@Stateless
+public class HistoricalRecordService {
+
+    @EJB
+    private HistoricalRecordFacade historicalRecordFacade;
+
+    public HistoricalRecord findRecord(String variableName, Institution institution, Institution site, Department department, Date recordDate) {
+        String jpql = "select hr "
+                + " from HistoricalRecord hr "
+                + " where hr.retired=false "
+                + " and hr.variableName=:vn "
+                + " and hr.recordDate=:rd ";
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("vn", variableName);
+        parameters.put("rd", recordDate);
+
+        if (institution != null) {
+            jpql += " and hr.institution=:ins ";
+            parameters.put("ins", institution);
+        } else {
+            jpql += " and hr.institution is null ";
+        }
+
+        if (site != null) {
+            jpql += " and hr.site=:site ";
+            parameters.put("site", site);
+        } else {
+            jpql += " and hr.site is null ";
+        }
+        if (department != null) {
+            jpql += " and hr.department=:dep ";
+            parameters.put("dep", department);
+        } else {
+            jpql += " and hr.department is null ";
+        }
+        HistoricalRecord r = historicalRecordFacade.findFirstByJpql(jpql, parameters, TemporalType.DATE);
+        return r;
+    }
+
+    public List<String> fetchVariableNames() {
+        String jpql = "select distinct(hr.variableName) "
+                + " from HistoricalRecord hr "
+                + " where hr.retired=false "
+                + " order by hr.variableName";
+        return historicalRecordFacade.findStringListByJpql(jpql);
+    }
+
+}
