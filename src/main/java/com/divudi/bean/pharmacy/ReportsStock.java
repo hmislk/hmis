4a3f9118fddaf10794ed7e9705d73ac2a7763222
Dr M H Buddhika Ariyaratne <buddhika.ari@gmail.com>
2025-05-10 03:11:02 +0530

    Closes #12243 Signed-off-by: Dr M H Buddhika Ariyaratne <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/bean/pharmacy/ReportsStock.java
+++ src/main/java/com/divudi/bean/pharmacy/ReportsStock.java
@@ -82,6 +82,8 @@ public class ReportsStock implements Serializable {
     double totalPurchaseValue;
     double totalRetailSaleRate;
     double totalRetailSaleValue;
+    private int fromRecord;
+    private int toRecord;
     Vmp vmp;
     BillType[] billTypes;
     ReportKeyWord reportKeyWord;
@@ -162,6 +164,29 @@ public class ReportsStock implements Serializable {
         }, PharmacyReports.STOCK_REPORT_BY_BATCH, sessionController.getLoggedUser());
     }
 
+    
+    public void fillDepartmentStocksForDownload() {
+        reportTimerController.trackReportExecution(() -> {
+            System.out.println("fillDepartmentStocks");
+            Date startedAt = new Date();
+            System.out.println("startedAt = " + startedAt);
+            if (department == null) {
+                JsfUtil.addErrorMessage("Please select a department");
+                return;
+            }
+            Map<String, Object> m = new HashMap<>();
+            String sql = "select s from Stock s "
+                    + " where s.department=:d "
+                    + " and s.stock > 0 "
+                    + " order by s.id";
+            m.put("d", department);
+            Date beforeJpql = new Date();
+            System.out.println("beforeJpql = " + beforeJpql);
+            stocks = getStockFacade().findByJpql(sql, m, fromRecord, toRecord);
+        }, PharmacyReports.STOCK_REPORT_BY_BATCH, sessionController.getLoggedUser());
+    }
+
+    
     public void fillDepartmentStocksOfMedicines() {
         if (department == null) {
             JsfUtil.addErrorMessage("Please select a department");
@@ -1428,5 +1453,25 @@ public class ReportsStock implements Serializable {
     public void setBills(List<BillItem> billItems) {
         this.billItems = billItems;
     }
+
+    public int getFromRecord() {
+        return fromRecord;
+    }
+
+    public void setFromRecord(int fromRecord) {
+        this.fromRecord = fromRecord;
+    }
+
+    public int getToRecord() {
+        return toRecord;
+    }
+
+    public void setToRecord(int toRecord) {
+        this.toRecord = toRecord;
+    }
+
+   
+    
+    
 
 }
