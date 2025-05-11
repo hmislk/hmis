61f43b2d9b90adf8fbb284d5e44ede4a55c94651
buddhika <budd@dsa.com>
2025-05-11 11:18:45 +0530

    Error Handler Created

---
+++ src/main/java/com/divudi/bean/common/ErrorHandler.java
@@ -0,0 +1,58 @@
+package com.divudi.bean.common;
+
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+/**
+ * Error Handler Bean
+ * Created with assistance from ChatGPT by Dr M H B Ariyaratne
+ */
+@Named
+@RequestScoped
+public class ErrorHandler implements Serializable {
+
+    public String getStatusCode() {
+        Object code = FacesContext.getCurrentInstance().getExternalContext()
+                .getRequestMap().get("javax.servlet.error.status_code");
+        return code != null ? code.toString() : "N/A";
+    }
+
+    public String getExceptionType() {
+        Object type = FacesContext.getCurrentInstance().getExternalContext()
+                .getRequestMap().get("javax.servlet.error.exception_type");
+        return type != null ? type.toString() : "N/A";
+    }
+
+    public String getMessage() {
+        Object msg = FacesContext.getCurrentInstance().getExternalContext()
+                .getRequestMap().get("javax.servlet.error.message");
+        return msg != null ? msg.toString() : "N/A";
+    }
+
+    public String getRequestURI() {
+        Object uri = FacesContext.getCurrentInstance().getExternalContext()
+                .getRequestMap().get("javax.servlet.error.request_uri");
+        return uri != null ? uri.toString() : "N/A";
+    }
+
+    public String getException() {
+        Throwable t = (Throwable) FacesContext.getCurrentInstance().getExternalContext()
+                .getRequestMap().get("javax.servlet.error.exception");
+        return t != null ? t.toString() : "N/A";
+    }
+
+    public String getStackTrace() {
+        Throwable t = (Throwable) FacesContext.getCurrentInstance().getExternalContext()
+                .getRequestMap().get("javax.servlet.error.exception");
+        if (t == null) {
+            return "N/A";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (StackTraceElement element : t.getStackTrace()) {
+            sb.append(element.toString()).append("\n");
+        }
+        return sb.toString();
+    }
+}
