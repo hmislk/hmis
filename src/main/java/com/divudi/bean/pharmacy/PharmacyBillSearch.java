85671c123c85a1a073eba64a3ae0c23636f6c90a
Lawan Samarasekara <lawan.chaamindu1234@gmail.com>
2025-06-04 18:12:19 +0530

    Signed-off-by: Lawan Samarasekara <lawan.chaamindu1234@gmail.com>

--- src/main/java/com/divudi/bean/pharmacy/PharmacyBillSearch.java
+++ src/main/java/com/divudi/bean/pharmacy/PharmacyBillSearch.java
@@ -2284,6 +2284,7 @@ public class PharmacyBillSearch implements Serializable {
         newlyCreatedCancellationBill.setDeptId(deptId);
         newlyCreatedCancellationBill.setReferenceBill(getBill());
         getBillFacade().edit(newlyCreatedCancellationBill);
+        billService.createBillFinancialDetailsForPharmacyBill(newlyCreatedCancellationBill);
 
         getBill().setCancelled(true);
         getBill().setCancelledBill(newlyCreatedCancellationBill);
@@ -2324,11 +2325,11 @@ public class PharmacyBillSearch implements Serializable {
         newlyCreatedCancellationBill.setDeptId(deptId);
         newlyCreatedCancellationBill.setReferenceBill(getBill());
         getBillFacade().edit(newlyCreatedCancellationBill);
+        billService.createBillFinancialDetailsForPharmacyBill(newlyCreatedCancellationBill);
 
         getBill().setCancelled(true);
         getBill().setCancelledBill(newlyCreatedCancellationBill);
         getBillFacade().edit(getBill());
-        billService.createBillFinancialDetailsForPharmacyBill(newlyCreatedCancellationBill);
         JsfUtil.addSuccessMessage("Cancelled");
 
         printPreview = true;
