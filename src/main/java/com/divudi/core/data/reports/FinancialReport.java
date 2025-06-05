3c679ce2a749cfaaabb9445789701779ecc000d4
imexh <chandikaranawella@gmail.com>
2025-06-05 09:36:19 +0530

    Added report log method to opd and inward due report

--- src/main/java/com/divudi/core/data/reports/FinancialReport.java
+++ src/main/java/com/divudi/core/data/reports/FinancialReport.java
@@ -3,7 +3,7 @@ package com.divudi.core.data.reports;
 public enum FinancialReport implements IReportType {
     STAFF_WELFARE_REPORT("Staff Welfare Report"),
     DAILY_RETURN("Daily Return"),
-    OPD_AND_INWARD_DUE_REPORT("OPD and Inward Due Report"),;
+    OPD_AND_INWARD_DUE_REPORT("OPD and Inward Due Report");
 
     private final String displayName;
 
