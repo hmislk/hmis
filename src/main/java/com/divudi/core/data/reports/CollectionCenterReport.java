3a9e6714d4e3c9a4f6c92b9f45c7540c48e54d8d
KithminiOKD <112233369+KithminiOKD@users.noreply.github.com>
2025-06-09 22:16:33 +0530

    CollectionCenterReport.java

--- src/main/java/com/divudi/core/data/reports/CollectionCenterReport.java
+++ src/main/java/com/divudi/core/data/reports/CollectionCenterReport.java
@@ -6,7 +6,10 @@ public enum CollectionCenterReport implements IReportType {
     COLLECTION_CENTER_REPORTS_PRINT("Collection Centre Reports Print"),
     COLLECTION_CENTER_BALANCE_REPORT("Collection Centre Balance Report"),
     COLLECTION_CENTER_RECEIPT_REPORT("Collection Centre Receipt Report"),
-    COLLECTION_CENTER_TEST_WISE_COUNT_REPORT("Collection Center Test Wise Count Report");
+    COLLECTION_CENTER_TEST_WISE_COUNT_REPORT("Collection Center Test Wise Count Report"),
+    COLLECTION_CENTER_BILL_WISE_DETAIL_REPORT("Collection center bill wise detail Report"),
+    COLLECTION_CENTER_WISE_INVOICE_LIST_REPORT("Collection center wise invoice list Report"),
+    COLLECTION_CENTER_BOOK_WISE_DETAIL_REPORT("Collection center book wise detail Report");
 
     private final String displayName;
 
