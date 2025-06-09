5454f53b1ee4b83ce5e3dedb702735fee7358b76
KithminiOKD <112233369+KithminiOKD@users.noreply.github.com>
2025-06-09 22:17:38 +0530

    LaboratoryReport.java

--- src/main/java/com/divudi/core/data/reports/LaboratoryReport.java
+++ src/main/java/com/divudi/core/data/reports/LaboratoryReport.java
@@ -2,7 +2,8 @@ package com.divudi.core.data.reports;
 
 public enum LaboratoryReport implements IReportType {
     SAMPLE_CARRIER_REPORT("Sample Carrier Report"),
-    INVESTIGATION_MONTH_END_SUMMARY("Investigation Month End Summary");
+    INVESTIGATION_MONTH_END_SUMMARY("Investigation Month End Summary"),
+    EXTERNAL_LABORATORY_WORKLOAD_REPORT("External laboratory workload report");
 
     private final String displayName;
 
