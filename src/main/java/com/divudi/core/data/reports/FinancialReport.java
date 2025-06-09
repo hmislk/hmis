4d5b3eb783e6904b93ffa012cc7ad84c4a094ede
KithminiOKD <112233369+KithminiOKD@users.noreply.github.com>
2025-06-09 22:17:20 +0530

    FinancialReport.java

--- src/main/java/com/divudi/core/data/reports/FinancialReport.java
+++ src/main/java/com/divudi/core/data/reports/FinancialReport.java
@@ -4,7 +4,13 @@ public enum FinancialReport implements IReportType {
     STAFF_WELFARE_REPORT("Staff Welfare Report"),
     DAILY_RETURN("Daily Return"),
     OPD_AND_INWARD_DUE_REPORT("OPD and Inward Due Report"),
-    DEBTOR_BALANCE_REPORT("Debtor Balance Report");
+    DEBTOR_BALANCE_REPORT("Debtor Balance Report"),
+    DISCOUNT_REPORT("Discount Report"),
+    INVOICE_AND_RECEIPT_REPORT("Invoice and receipt Report"),
+    CREDIT_INVOICE_DISPATCH_REPORT("Credit Invoice dispatch Report"),
+    PAYMENT_SETTLEMENT_REPORT("Payment Settlement Report"),
+    PACKAGE_REPORT("Package Report"),
+    INWARD_DUE_SEARCH("Inward Due Search"),;
 
     private final String displayName;
 
