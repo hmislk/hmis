70d5af55fe7a491f6d05770b080c289b3bbb0f94
buddhika <buddhika.ari@gmail.com>
2025-06-27 18:51:51 +0530

    Signed-off-by: buddhika <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/core/entity/ConfigOption.java
+++ src/main/java/com/divudi/core/entity/ConfigOption.java
@@ -12,9 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import javax.persistence.Temporal;
-import javax.persistence.UniqueConstraint;
 
 /**
  *
