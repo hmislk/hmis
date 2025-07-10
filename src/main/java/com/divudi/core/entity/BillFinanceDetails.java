8c41c46c87efebe3f15d82af41f84e2c99b62203
Dr M H Buddhika Ariyaratne <buddhika.ari@gmail.com>
2025-07-11 02:35:45 +0530

    Signed-off-by: Dr M H Buddhika Ariyaratne <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/core/entity/BillFinanceDetails.java
+++ src/main/java/com/divudi/core/entity/BillFinanceDetails.java
@@ -3,7 +3,6 @@ package com.divudi.core.entity;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
-import javax.enterprise.context.Dependent;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
