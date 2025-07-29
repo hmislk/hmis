0e326013ce2aa21fea82cb8e8a9b4281a619ee5a
DamithDeshan <hkddrajapaksha@gmail.com>
2025-07-29 15:49:53 +0530

    Fixed - codeRabbit Suggetion Signed-off-by: DamithDeshan <hkddrajapaksha@gmail.com>

--- src/main/java/com/divudi/bean/opd/OpdBillController.java
+++ src/main/java/com/divudi/bean/opd/OpdBillController.java
@@ -1808,11 +1808,11 @@ public class OpdBillController implements Serializable, ControllerWithPatient, C
                 JsfUtil.addErrorMessage("Please Enter a Credit Card Comment..");
                 error = true;
             }
-            if (getPaymentMethodData().getCreditCard().getNo().trim().equals("") && configOptionApplicationController.getBooleanValueByKey("OPD Billing - CreditCard No is Mandatory",true)) {
+            if ((getPaymentMethodData().getCreditCard().getNo() == null || getPaymentMethodData().getCreditCard().getNo().trim().equals("")) && configOptionApplicationController.getBooleanValueByKey("OPD Billing - CreditCard No is Mandatory",true)) {
                 JsfUtil.addErrorMessage("Please Enter a Credit Card No.");
                 error = true;
             }
-            if (getPaymentMethodData().getCreditCard().getNo().length() < 4  && configOptionApplicationController.getBooleanValueByKey("OPD Billing - Last 4 number of Credit Card is Mandatory.", true)) {
+            if (getPaymentMethodData().getCreditCard().getNo() != null && getPaymentMethodData().getCreditCard().getNo().length() < 4  && configOptionApplicationController.getBooleanValueByKey("OPD Billing - Last 4 number of Credit Card is Mandatory.", true)) {
                 JsfUtil.addErrorMessage("Please Enter Last 4 number of Credit Card.");
                 error = true;
             }
