6370573f67011a33b260e13b3cc4fc91ae398cd9
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2025-09-17 14:38:21 +0530

    Closes #14889 Signed-off-by: Dr M H B Ariyaratne <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/bean/common/SearchController.java
+++ src/main/java/com/divudi/bean/common/SearchController.java
@@ -15575,7 +15575,7 @@ public class SearchController implements Serializable {
             opdRefunds.add(BillTypeAtomic.OPD_BILL_REFUND);
 
             // Generate OPD service collection and add to the main bundle
-            ReportTemplateRowBundle opdServiceCollection = generatePaymentMethodColumnsByBills(opdBts, nonCreditPaymentMethods);
+            ReportTemplateRowBundle opdServiceCollection = generatePaymentColumnForCollections(opdBts, nonCreditPaymentMethods);
             opdServiceCollection.setBundleType("cashierSummaryOpd");
             opdServiceCollection.setName("OPD Collection");
             bundle.getBundles().add(opdServiceCollection);
@@ -15622,7 +15622,7 @@ public class SearchController implements Serializable {
             pharmacyCollectionBillTypes.add(BillTypeAtomic.PHARMACY_RETAIL_SALE_PREBILL_SETTLED_AT_CASHIER);
             pharmacyCollectionBillTypes.add(BillTypeAtomic.PHARMACY_WHOLESALE);
             pharmacyCollectionBillTypes.add(BillTypeAtomic.PHARMACY_SALE_WITHOUT_STOCK);
-            ReportTemplateRowBundle pharmacyCollection = generatePaymentMethodColumnsByBills(pharmacyCollectionBillTypes, nonCreditPaymentMethods);
+            ReportTemplateRowBundle pharmacyCollection = generateTotalPaymentColumnByDepartment(pharmacyCollectionBillTypes, nonCreditPaymentMethods);
             pharmacyCollection.setBundleType("pharmacyCollection");
             pharmacyCollection.setName("Pharmacy Collection");
             bundle.getBundles().add(pharmacyCollection);
@@ -15870,6 +15870,13 @@ public class SearchController implements Serializable {
             bundle.getBundles().add(pharmacyCreditRefundBundle);
             collectionForTheDay += getSafeTotal(pharmacyCreditRefundBundle);
 
+            // Final net cash for the day
+            ReportTemplateRowBundle netCashForTheDayBundle = new ReportTemplateRowBundle();
+            netCashForTheDayBundle.setName("Net Cash");
+            netCashForTheDayBundle.setBundleType("netCash");
+            netCashForTheDayBundle.setTotal(netCashCollection);
+
+            bundle.getBundles().add(netCashForTheDayBundle);
             bundle.calculateTotalsByAllChildBundles();
         }, CashierReports.CASHIER_SUMMARY, sessionController.getLoggedUser());
     }
@@ -16268,6 +16275,13 @@ public class SearchController implements Serializable {
             bundle.getBundles().add(ChannelBookingsRefundBundle);
             collectionForTheDay -= getSafeTotal(ChannelBookingsRefundBundle);
 
+            // Final net cash for the day
+            ReportTemplateRowBundle netCashForTheDayBundle = new ReportTemplateRowBundle();
+            netCashForTheDayBundle.setName("Net Cash");
+            netCashForTheDayBundle.setBundleType("netCash");
+            netCashForTheDayBundle.setTotal(netCashCollection);
+
+            bundle.getBundles().add(netCashForTheDayBundle);
             bundle.calculateTotalsByAllChildBundles();
         }, CashierReports.CASHIER_DETAILED, sessionController.getLoggedUser());
 
