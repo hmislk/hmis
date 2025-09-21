816138631d9736d4df78ffd2781d1a26a5ead2c3
Dr M H Buddhika Ariyaratne <buddhika.ari@gmail.com>
2025-09-22 01:04:31 +0530

    Closes #4083 Signed-off-by: Dr M H Buddhika Ariyaratne <buddhika.ari@gmail.com>

--- src/main/webapp/resources/sql/bills.sql
+++ src/main/webapp/resources/sql/bills.sql
@@ -1,7 +1,9 @@
-select `ID`, `DTYPE`, `BILLTYPEATOMIC`, `DEPTID`, `NETTOTAL` , `PAYMENTMETHOD`, `REFERENCEBILL_ID`,  `DEPARTMENT_ID`, `INSTITUTION_ID`, `FROMINSTITUTION_ID`, `TOINSTITUTION_ID`
+select `ID`, `DTYPE`, `BILLTYPE`, `BILLTYPEATOMIC`, `DEPTID`, `NETTOTAL` , `PAYMENTMETHOD`, `REFERENCEBILL_ID`,  `DEPARTMENT_ID`, `INSTITUTION_ID`, `FROMINSTITUTION_ID`, `TOINSTITUTION_ID`
 from bill 
 order by `ID` desc limit 10;
-select * from billitemfinancedetails order by id desc limit 1;
+
+select * from billitem order by id desc limit 10;
+select * from billitemfinancedetails order by id desc limit 10;
 select `ID`, `QTY`, `FREEQTY` from pharmaceuticalbillitem order by id desc limit 10;
 
 select * from stockhistory order by id desc limit 50;
