1f4c200901a5e171ffa187fc1c70fa5d651e49a9
Pubudu <pubudupiyankara.me@gmail.com>
2025-10-08 20:54:10 +0530

    Signed-off-by: Pubudu <pubudupiyankara.me@gmail.com>

--- src/main/java/com/divudi/bean/report/PharmacyReportController.java
+++ src/main/java/com/divudi/bean/report/PharmacyReportController.java
@@ -3606,6 +3606,20 @@ public class PharmacyReportController implements Serializable {
 //            billTypeAtomics.add(BillTypeAtomic.PHARMACY_RECEIVE_CANCELLED);
             } else if ("returnWithoutReceiptDoc".equals(documentType)) {
                 billTypeAtomics.add(BillTypeAtomic.PHARMACY_RETURN_WITHOUT_TREASING);
+            } else if ("adjustmentDoc".equals(documentType)) {
+                billTypeAtomics.add(BillTypeAtomic.PHARMACY_STOCK_ADJUSTMENT);
+                billTypeAtomics.add(BillTypeAtomic.PHARMACY_STOCK_ADJUSTMENT_BILL);
+
+                billTypeAtomics.add(BillTypeAtomic.PHARMACY_STOCK_EXPIRY_DATE_AJUSTMENT);
+                billTypeAtomics.add(BillTypeAtomic.PHARMACY_ADJUSTMENT);
+                billTypeAtomics.add(BillTypeAtomic.PHARMACY_ADJUSTMENT_CANCELLED);
+                billTypeAtomics.add(BillTypeAtomic.PHARMACY_PURCHASE_RATE_ADJUSTMENT);
+
+                billTypeAtomics.add(BillTypeAtomic.PHARMACY_RETAIL_RATE_ADJUSTMENT);
+                billTypeAtomics.add(BillTypeAtomic.PHARMACY_COST_RATE_ADJUSTMENT);
+                billTypeAtomics.add(BillTypeAtomic.PHARMACY_WHOLESALE_RATE_ADJUSTMENT);
+                billTypeAtomics.add(BillTypeAtomic.PHARMACY_STAFF_STOCK_ADJUSTMENT);
+                billTypeAtomics.add(BillTypeAtomic.PHARMACY_STOCK_EXPIRY_DATE_AJUSTMENT);
             } else {
                 billTypes.add(BillType.PharmacyBhtPre);
 
