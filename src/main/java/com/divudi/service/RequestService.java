adc4355325b913fc4a86fe5509664ef796b35b0c
DamithDeshan <hkddrajapaksha@gmail.com>
2025-10-16 15:24:27 +0530

    Update RequestService class
    Signed-off-by: DamithDeshan <hkddrajapaksha@gmail.com>

--- src/main/java/com/divudi/service/RequestService.java
+++ src/main/java/com/divudi/service/RequestService.java
@@ -1,9 +1,15 @@
 package com.divudi.service;
 
+import com.divudi.core.data.RequestStatus;
+import com.divudi.core.data.RequestType;
+import com.divudi.core.entity.Bill;
+import com.divudi.core.entity.BillItem;
 import com.divudi.core.entity.Request;
 import com.divudi.core.entity.WebUser;
 import com.divudi.core.facade.RequestFacade;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
@@ -17,10 +23,6 @@ public class RequestService {
     @EJB
     RequestFacade requestFacade;
 
-    public void save(Request req) {
-        save(req, null);
-    }
-
     public void save(Request req, WebUser user) {
         if (req == null) {
             return;
@@ -33,6 +35,24 @@ public class RequestService {
         } else {
             requestFacade.edit(req);
         }
+    }
+    
+    public Request findRequest(Bill bill) {
+        System.out.println("RequestService = " + "findRequest");
+        String jpql;
+        HashMap params = new HashMap();
+        
+        jpql = "Select q from Request q "
+                + " where q.retired =:ret "
+                + " and q.bill =:bill "
+                + " and q.status =:status ";
+        params.put("ret",false);
+        params.put("bill",bill);
+        params.put("status",RequestStatus.PENDING);
+        
+        Request req = requestFacade.findFirstByJpql(jpql, params);
+        System.out.println("req = " + req);
+        return req;
     }
 
 }
