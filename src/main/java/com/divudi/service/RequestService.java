9d18add904e8604673ec906e4b2488b94e26da3c
DamithDeshan <hkddrajapaksha@gmail.com>
2025-10-17 15:16:20 +0530

    Improve RequestService Signed-off-by: DamithDeshan <hkddrajapaksha@gmail.com>

--- src/main/java/com/divudi/service/RequestService.java
+++ src/main/java/com/divudi/service/RequestService.java
@@ -1,9 +1,9 @@
 package com.divudi.service;
 
+import com.divudi.core.data.DepartmentType;
 import com.divudi.core.data.RequestStatus;
 import com.divudi.core.data.RequestType;
 import com.divudi.core.entity.Bill;
-import com.divudi.core.entity.BillItem;
 import com.divudi.core.entity.Request;
 import com.divudi.core.entity.WebUser;
 import com.divudi.core.facade.RequestFacade;
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.persistence.TemporalType;
 
 /**
  * @author H.K. Damith Deshan | hkddrajapaksha@gmail.com
@@ -43,26 +44,53 @@ public class RequestService {
             return null;
         }
         
-        System.out.println("RequestService = " + "findRequest");
-        String jpql;
-        
         List<RequestStatus> availableStatus = new ArrayList<>();
         availableStatus.add(RequestStatus.PENDING);
         availableStatus.add(RequestStatus.UNDER_REVIEW);
         availableStatus.add(RequestStatus.APPROVED);
         
-        HashMap params = new HashMap();
-        
-        jpql = "Select q from Request q "
+        String jpql = "Select q from Request q "
                 + " where q.retired =:ret "
                 + " and q.bill =:bill "
                 + " and q.status in :status ";
+        
+        HashMap params = new HashMap();
         params.put("ret",false);
         params.put("bill",bill);
         params.put("status",availableStatus);
         
         Request req = requestFacade.findFirstByJpql(jpql, params);
+
+        return req;
+    }
+    
+    public List<Request> fillAllRequest(Date fromDate, Date toDate, RequestType requestType, RequestStatus requestStatus, DepartmentType departmentType){
+        
+        HashMap params = new HashMap();
+        
+        String jpql = "Select q from Request q "
+                + " where q.retired =:ret "
+                + " and q.createdAt between :frm and :to"
+                + " and q.department.departmentType =:deptType";
+                
+        if(requestStatus != null){
+            jpql += " and q.status = :status ";
+            params.put("status",requestStatus);
+        }
+        if(requestType != null){
+            jpql += " and q.requestType = :type ";
+            params.put("type",requestType);
+        }
+        
+        params.put("deptType",departmentType);
+        params.put("ret",false);
+        params.put("frm",fromDate);
+        params.put("to",toDate);
+        
+        List<Request> req = requestFacade.findByJpql(jpql, params, TemporalType.TIMESTAMP);
+        
         System.out.println("req = " + req);
+        
         return req;
     }
 
