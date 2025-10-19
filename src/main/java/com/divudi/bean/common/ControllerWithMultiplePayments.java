2ce49f310e36843ee23a8457ec4a79b4b1abca0f
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2025-10-19 13:35:25 +0530

    Signed-off-by: Dr M H B Ariyaratne <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/bean/common/ControllerWithMultiplePayments.java
+++ src/main/java/com/divudi/bean/common/ControllerWithMultiplePayments.java
@@ -2,6 +2,7 @@ package com.divudi.bean.common;
 
 import com.divudi.core.data.dataStructure.ComponentDetail;
 import com.divudi.core.data.dataStructure.PaymentMethodData;
+import java.util.List;
 
 /**
  * @author Senula Nanayakkara
@@ -19,8 +20,26 @@ public interface ControllerWithMultiplePayments {
      * Checks if the given ComponentDetail is the last payment entry in the multiple payment methods list.
      * This is used to determine which payment fields should be editable in the UI.
      *
+     * Uses position-based comparison (indexOf) instead of equals() because ComponentDetail
+     * does not implement equals/hashCode. This ensures proper identification of the last entry
+     * even when objects are deserialized or recreated during JSF lifecycle.
+     *
      * @param cd The ComponentDetail to check
-     * @return true if cd is the last entry, false otherwise
+     * @return true if cd is the last entry in the list, false otherwise
      */
-    boolean isLastPaymentEntry(ComponentDetail cd);
+    default boolean isLastPaymentEntry(ComponentDetail cd) {
+        PaymentMethodData pmd = getPaymentMethodData();
+        if (cd == null ||
+            pmd == null ||
+            pmd.getPaymentMethodMultiple() == null ||
+            pmd.getPaymentMethodMultiple().getMultiplePaymentMethodComponentDetails() == null ||
+            pmd.getPaymentMethodMultiple().getMultiplePaymentMethodComponentDetails().isEmpty()) {
+            return false;
+        }
+
+        List<ComponentDetail> details = pmd.getPaymentMethodMultiple().getMultiplePaymentMethodComponentDetails();
+        int lastIndex = details.size() - 1;
+        int currentIndex = details.indexOf(cd);
+        return currentIndex != -1 && currentIndex == lastIndex;
+    }
 }
