8b61554d9cfcd65dd702bdcdb4e1e3242efaaf22
ChinthakaPrasad <chinthakaprasad30@gmail.com>
2025-10-29 11:27:10 +0530

    fix patient state issue in inward admission
    Signed-off-by: ChinthakaPrasad <chinthakaprasad30@gmail.com>

--- src/main/java/com/divudi/bean/inward/BhtEditController.java
+++ src/main/java/com/divudi/bean/inward/BhtEditController.java
@@ -66,6 +66,8 @@ public class BhtEditController implements Serializable, ControllerWithPatient {
     SessionController sessionController;
     @Inject
     BillBeanController billBean;
+    @Inject
+    private AdmissionController admissionController;
     /////////////
     @EJB
     private AdmissionFacade ejbFacade;
@@ -479,6 +481,8 @@ public class BhtEditController implements Serializable, ControllerWithPatient {
             JsfUtil.addErrorMessage("No Admission to edit");
             return "";
         }
+        
+        admissionController.setCurrent(current);
         createPatientRoom();
         fillCreditCompaniesByPatient();
         fillCurrentPatientAllergies(current.getPatient());
