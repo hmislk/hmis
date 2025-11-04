f515598f0fd276b5ba40f71b7452c7be587eda2e
DamithDeshan <hkddrajapaksha@gmail.com>
2025-11-04 14:21:47 +0530

    Create a DTO Signed-off-by: DamithDeshan <hkddrajapaksha@gmail.com>

---
+++ src/main/java/com/divudi/core/data/dto/PatientRoomDTO.java
@@ -0,0 +1,66 @@
+package com.divudi.core.data.dto;
+
+import java.io.Serializable;
+
+public class PatientRoomDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private Long id;
+    private Long admissionId;
+    private String bhtNo;
+    private String currentRoomNo;
+    private Boolean inPatient;
+
+    public PatientRoomDTO() {
+    }
+
+    public PatientRoomDTO(Long id, Long admissionId, String bhtNo, String currentRoomNo, Boolean inPatient) {
+        this.id = id;
+        this.admissionId = admissionId;
+        this.bhtNo = bhtNo;
+        this.currentRoomNo = currentRoomNo;
+        this.inPatient = inPatient;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAdmissionId() {
+        return admissionId;
+    }
+
+    public void setAdmissionId(Long admissionId) {
+        this.admissionId = admissionId;
+    }
+
+    public String getBhtNo() {
+        return bhtNo;
+    }
+
+    public void setBhtNo(String bhtNo) {
+        this.bhtNo = bhtNo;
+    }
+
+    public String getCurrentRoomNo() {
+        return currentRoomNo;
+    }
+
+    public void setCurrentRoomNo(String currentRoomNo) {
+        this.currentRoomNo = currentRoomNo;
+    }
+
+    public Boolean getInPatient() {
+        return inPatient;
+    }
+
+    public void setInPatient(Boolean inPatient) {
+        this.inPatient = inPatient;
+    }
+
+}
+
