25789a6f4aa258dad8badd161eaf9f6708605c4b
PasinduW99 <pwathsara@gmail.com>
2025-11-07 18:42:32 +0530

    Signed-off-by: PasinduW99 <pwathsara@gmail.com>

--- src/main/java/com/divudi/core/data/Privileges.java
+++ src/main/java/com/divudi/core/data/Privileges.java
@@ -110,6 +110,7 @@ public enum Privileges {
     
     //<editor-fold defaultstate="collapsed" desc="Nurse">
     NursingWorkBench("Nursing Work Bench"),
+    ShowDrugCharges("Show Drug Charges"),
     //</editor-fold>
 
     //<editor-fold defaultstate="collapsed" desc="Finance">
