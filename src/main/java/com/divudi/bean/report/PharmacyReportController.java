2aca4b46a485d41b4aa89527e73d0a53d2aee909
Pubudu <pubudupiyankara.me@gmail.com>
2025-11-16 01:51:23 +0530

    Signed-off-by: Pubudu <pubudupiyankara.me@gmail.com>

--- src/main/java/com/divudi/bean/report/PharmacyReportController.java
+++ src/main/java/com/divudi/bean/report/PharmacyReportController.java
@@ -2016,6 +2016,12 @@ public class PharmacyReportController implements Serializable {
                 BillTypeAtomic.PHARMACY_DIRECT_PURCHASE
         );
         retrieveBillItems(btas, getNonCreditPaymentMethods());
+        totalCostValue = 0.0;
+        totalCostValue = billItems.stream()
+                    .map(BillItem::getBillItemFinanceDetails)
+                    .distinct()
+                    .mapToDouble(details -> details.getTotalCost().doubleValue())
+                    .sum();
     }
 
     public void processGrnCredit() {
