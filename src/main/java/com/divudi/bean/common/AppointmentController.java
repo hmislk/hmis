f938fec1131ce796bc0b9e83409928e886a441e4
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2025-12-10 22:34:48 +0530

    Signed-off-by: Dr M H B Ariyaratne <buddhika.ari@gmail.com>

--- src/main/java/com/divudi/bean/common/AppointmentController.java
+++ src/main/java/com/divudi/bean/common/AppointmentController.java
@@ -1355,4 +1355,47 @@ public class AppointmentController implements Serializable, ControllerWithPatien
         }
     }
 
+    /**
+     * Converter for Reservation entity
+     */
+    @FacesConverter(forClass = Reservation.class)
+    public static class ReservationControllerConverter implements Converter {
+
+        @Override
+        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+            if (value == null || value.isEmpty()) {
+                return null;
+            }
+            AppointmentController controller = (AppointmentController) facesContext.getApplication().getELResolver().
+                    getValue(facesContext.getELContext(), null, "appointmentController");
+            return controller.getReservationFacade().find(getKey(value));
+        }
+
+        java.lang.Long getKey(String value) {
+            java.lang.Long key;
+            key = Long.valueOf(value);
+            return key;
+        }
+
+        String getStringKey(java.lang.Long value) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(value);
+            return sb.toString();
+        }
+
+        @Override
+        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+            if (object == null) {
+                return null;
+            }
+            if (object instanceof Reservation) {
+                Reservation o = (Reservation) object;
+                return getStringKey(o.getId());
+            } else {
+                throw new IllegalArgumentException("object " + object + " is of type "
+                        + object.getClass().getName() + "; expected type: " + Reservation.class.getName());
+            }
+        }
+    }
+
 }
