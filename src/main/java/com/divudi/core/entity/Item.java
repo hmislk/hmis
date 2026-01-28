f5c814aa28f7d5468cc943c5987818dc15052957
Dhanesh Kaushan <dhaneshkaushan@gmail.com>
2026-01-28 12:41:15 +0530

    Fix -#17918 Add text box for Surgery Type for surgery

--- src/main/java/com/divudi/core/entity/Item.java
+++ src/main/java/com/divudi/core/entity/Item.java
@@ -87,6 +87,8 @@ public class Item implements Serializable, Comparable<Item>, RetirableEntity {
     @ManyToOne
     Category category;
     @ManyToOne
+    private Category surgeryType;
+    @ManyToOne
     private Category financialCategory;
     Double total = 0.0;
     private Double totalForForeigner = 0.0;
@@ -306,7 +308,7 @@ public class Item implements Serializable, Comparable<Item>, RetirableEntity {
 
     @Column(nullable = false)
     private boolean consumptionAllowed = true;
-    
+
     private boolean allowedForBillingPriority;
 
     public double getVatPercentage() {
@@ -1665,6 +1667,14 @@ public class Item implements Serializable, Comparable<Item>, RetirableEntity {
 
     public void setAllowedForBillingPriority(boolean allowedForBillingPriority) {
         this.allowedForBillingPriority = allowedForBillingPriority;
+    }
+
+    public Category getSurgeryType() {
+        return surgeryType;
+    }
+
+    public void setSurgeryType(Category surgeryType) {
+        this.surgeryType = surgeryType;
     }
 
     static class ReportItemComparator implements Comparator<ReportItem> {
