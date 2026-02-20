a0d50fec192927903c1e70ca02529ea5d7b4930c
DamithDeshan <hkddrajapaksha@gmail.com>
2026-02-20 09:09:26 +0530

    Signed-off-by: DamithDeshan <hkddrajapaksha@gmail.com>

--- src/main/java/com/divudi/service/SerialNumberGeneratorService.java
+++ src/main/java/com/divudi/service/SerialNumberGeneratorService.java
@@ -1,89 +1,53 @@
 package com.divudi.service;
 
-import com.divudi.core.data.dataStructure.SearchKeyword;
-import com.divudi.core.entity.lab.PatientInvestigation;
-import com.divudi.core.facade.PatientInvestigationFacade;
-import java.util.ArrayList;
+import com.divudi.core.data.BillType;
+import com.divudi.core.entity.BilledBill;
+import com.divudi.core.entity.Category;
+import com.divudi.core.entity.Department;
+import com.divudi.core.entity.PreBill;
+import com.divudi.core.entity.Staff;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.TemporalType;
 
 /**
- *
- * @author Dr M H B Ariyaratne buddhika.ari@gmail.com
- *
+ * @author H.K. Damith Deshan | hkddrajapaksha@gmail.com
  */
 @Stateless
-public class PatientInvestigationService {
+public class SerialNumberGeneratorService {
 
-    @EJB
-    PatientInvestigationFacade PatientInvestigationFacade;
+    public String generateDailyBillNumberForOpd(Department department, Category cat, Staff fromStaff) {
+        String sql = "SELECT count(b) FROM Bill b "
+                + " where (b.billType=:bTp1 or b.billType=:bTp2) "
+                + " and b.billDate=:bd "
+                + " and (type(b)=:class1 or type(b)=:class2) ";
+        HashMap hm = new HashMap();
 
-    public List<PatientInvestigation> fetchPatientInvestigations(
-            Date fromDate, Date toDate, SearchKeyword searchKeyword) {
-        List<PatientInvestigation> patientInvestigations = new ArrayList<>();
-
-        if (fromDate == null || toDate == null) {
-            return patientInvestigations;
+        if (department != null) {
+            sql += " and b.department=:dep ";
+            hm.put("dep", department);
         }
 
-        String jpql = "select pi "
-                + " from PatientInvestigation pi "
-                + " join pi.investigation i "
-                + " join pi.billItem.bill b "
-                + " join b.patient.person p "
-                + " where b.createdAt between :fromDate and :toDate ";
-
-        Map<String, Object> params = new HashMap<>();
-        params.put("fromDate", fromDate);
-        params.put("toDate", toDate);
-
-        // Only apply searchKeyword filters if searchKeyword is not null
-        if (searchKeyword != null) {
-            if (searchKeyword.getItemDepartment() != null) {
-                jpql += " and pi.investigation.department=:dep ";
-                params.put("dep", searchKeyword.getItemDepartment());
-            }
-
-            if (searchKeyword.getPatientName() != null && !searchKeyword.getPatientName().trim().isEmpty()) {
-                jpql += " and ((p.name) like :patientName)";
-                params.put("patientName", "%" + searchKeyword.getPatientName().trim().toUpperCase() + "%");
-            }
-
-            if (searchKeyword.getBillNo() != null && !searchKeyword.getBillNo().trim().isEmpty()) {
-                jpql += " and ((b.insId) like :billNo)";
-                params.put("billNo", "%" + searchKeyword.getBillNo().trim().toUpperCase() + "%");
-            }
-
-            if (searchKeyword.getPatientPhone() != null && !searchKeyword.getPatientPhone().trim().isEmpty()) {
-                jpql += " and ((p.phone) like :patientPhone)";
-                params.put("patientPhone", "%" + searchKeyword.getPatientPhone().trim().toUpperCase() + "%");
-            }
-
-            if (searchKeyword.getItemName() != null && !searchKeyword.getItemName().trim().isEmpty()) {
-                jpql += " and ((i.name) like :itm)";
-                params.put("itm", "%" + searchKeyword.getItemName().trim().toUpperCase() + "%");
-            }
-
-            if (searchKeyword.getInvestigation()!= null) {
-                jpql += " and i=:ix ";
-                params.put("ix", searchKeyword.getInvestigation());
-            }
-
-            if (searchKeyword.getPatientEncounter() != null) {
-                jpql += " and pi.encounter=:en";
-                params.put("en", searchKeyword.getPatientEncounter());
-            }
+        if (cat != null) {
+            sql += " and b.category=:cat ";
+            hm.put("cat", cat);
         }
 
-        jpql += " order by pi.id desc";
+        if (fromStaff != null) {
+            sql += " and b.fromStaff=:staff ";
+            hm.put("staff", fromStaff);
+        }
 
-        patientInvestigations = PatientInvestigationFacade.findByJpql(jpql, params, TemporalType.TIMESTAMP, 50);
-        return patientInvestigations;
+        hm.put("bTp1", BillType.OpdBill);
+        hm.put("bTp2", BillType.OpdPreBill);
+        hm.put("bd", new Date());
+        hm.put("class1", BilledBill.class);
+        hm.put("class2", PreBill.class);
+
+        Long dd = getBillFacade().findAggregateLong(sql, hm, TemporalType.DATE);
+        return (dd != null) ? String.valueOf(dd) : "0";
     }
+    
 
 }
