7974826d39fbc8c38d39f01ffbfcd648a48a2b90
damithdeshan98 <hkddrajapaksha@gmail.com>
2026-03-21 10:40:07 +0530

    Display the "Self Registered" in Profile in Patient Portal Signed-off-by: damithdeshan98 <hkddrajapaksha@gmail.com>

--- src/main/java/com/divudi/bean/channel/PatientPortalController.java
+++ src/main/java/com/divudi/bean/channel/PatientPortalController.java
@@ -224,14 +224,15 @@ public class PatientPortalController implements Serializable {
         patient.setPatientMobileNumber(phoneAsLong);
         patient.getPerson().setPhone(patientphoneNumber);
         patient.getPerson().setMobile(patientphoneNumber);
+        patient.setSelfRegistered(true);
         patientController.save(patient);
         addNewPatient = false;
         patientSelected = true;
         Long phoneAsLongForSearch = com.divudi.core.util.CommonFunctions.convertStringToLongOrZero(patientphoneNumber);
         java.util.Map<String, Object> searchMap = new java.util.HashMap<>();
         searchMap.put("pp", phoneAsLongForSearch);
-        searchedPatients = patientFacade.findByJpql(
-                "select p from Patient p where p.retired=false and p.patientPhoneNumber=:pp", searchMap);
+        searchedPatients = patientFacade.findByJpql("select p from Patient p where p.retired=false and p.patientPhoneNumber=:pp", searchMap);
+        
     }
 
     public void selectPatientProfile(Patient selectedPt) {
