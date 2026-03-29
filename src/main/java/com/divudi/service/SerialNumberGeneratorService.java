266fd6e16f288613f042e877e5bd394b740524a5
damithdeshan98 <hkddrajapaksha@gmail.com>
2026-03-29 07:46:39 +0530

    Generate Serial Number Signed-off-by: damithdeshan98 <hkddrajapaksha@gmail.com>

--- src/main/java/com/divudi/service/SerialNumberGeneratorService.java
+++ src/main/java/com/divudi/service/SerialNumberGeneratorService.java
@@ -1,11 +1,9 @@
 package com.divudi.service;
 
-import com.divudi.core.entity.Bill;
 import com.divudi.core.entity.Category;
 import com.divudi.core.entity.Department;
-import com.divudi.core.entity.Doctor;
-import com.divudi.core.entity.Institution;
 import com.divudi.core.entity.Item;
+import com.divudi.core.entity.Staff;
 import com.divudi.core.facade.BillItemFacade;
 import java.util.Calendar;
 import java.util.Date;
@@ -21,47 +19,45 @@ import javax.persistence.TemporalType;
  */
 @Stateless
 public class SerialNumberGeneratorService {
-    
+
     @EJB
     BillItemFacade billItemFacade;
-    
+
     private final ConcurrentHashMap<String, ReentrantLock> lockMap = new ConcurrentHashMap<>();
-    
-    
+
     // <editor-fold defaultstate="collapsed" desc="Lock Keys">
     // Bill
     private String getLockKey(Department department) {
         return department.getId().toString();
     }
-    
+
     // Category
     private String getLockKey(Department department, Category categorye) {
         return department.getId() + "-" + categorye.getId();
     }
-    
-    // Doctor
-    private String getLockKey(Department department, Doctor doctor) {
-        return department.getId() + "-" + doctor.getId();
+
+    // Staff
+    private String getLockKey(Department department, Staff staff) {
+        return department.getId() + "-" + staff.getId();
     }
-    
+
     //Item
     private String getLockKey(Department department, Item item) {
         return department.getId() + "-" + item.getId();
     }
-    
+
     //ItemDeDepartment
     private String getLockKey(Department department, Department itemDeDepartment) {
         return department.getId() + "-" + itemDeDepartment.getId();
     }
     // </editor-fold>
 
-    
     //ByBill
-    public String fetchLastSerialNumberForDayUsingBill(Department department){
+    public String fetchLastSerialNumberForDayUsingBill(Department department) {
         if (department == null) {
             return "";
         }
-        
+
         String lockKey = getLockKey(department);
         ReentrantLock lock = lockMap.computeIfAbsent(lockKey, k -> new ReentrantLock());
 
@@ -72,13 +68,13 @@ public class SerialNumberGeneratorService {
             lock.unlock();
         }
     }
-    
+
     //ByItem
-    public String fetchLastSerialNumberForDayUsingItem(Department department, Item item ){
+    public String fetchLastSerialNumberForDayUsingItem(Department department, Item item) {
         if (department == null || item == null) {
             return "";
         }
-        
+
         String lockKey = getLockKey(department, item);
         ReentrantLock lock = lockMap.computeIfAbsent(lockKey, k -> new ReentrantLock());
 
@@ -89,13 +85,13 @@ public class SerialNumberGeneratorService {
             lock.unlock();
         }
     }
-    
+
     //ByItemDepartment
-    public String fetchLastSerialNumberForDayUsingItemDeDepartment(Department department, Department itemDepartment){
+    public String fetchLastSerialNumberForDayUsingItemDeDepartment(Department department, Department itemDepartment) {
         if (department == null || itemDepartment == null) {
             return "";
         }
-        
+
         String lockKey = getLockKey(department, itemDepartment);
         ReentrantLock lock = lockMap.computeIfAbsent(lockKey, k -> new ReentrantLock());
 
@@ -106,13 +102,13 @@ public class SerialNumberGeneratorService {
             lock.unlock();
         }
     }
-    
+
     //ByCategory or BySubCategory
-    public String fetchLastSerialNumberForDayUsingCategory(Department department, Category category){
+    public String fetchLastSerialNumberForDayUsingCategory(Department department, Category category) {
         if (department == null || category == null) {
             return "";
         }
-        
+
         String lockKey = getLockKey(department, category);
         ReentrantLock lock = lockMap.computeIfAbsent(lockKey, k -> new ReentrantLock());
 
@@ -122,23 +118,29 @@ public class SerialNumberGeneratorService {
         } finally {
             lock.unlock();
         }
-        
+
     }
-    
-    //ByDoctor
-    public String fetchLastSerialNumberForDayUsingDoctor(Department department, Doctor doctor ){
-        return "";
+
+    //ByDoctor or //ByDoctorSession
+    public String fetchLastSerialNumberForDayUsingDoctor(Department department, Staff staff) {
+        if (department == null || staff == null) {
+            return "";
+        }
+
+        String lockKey = getLockKey(department, staff);
+        ReentrantLock lock = lockMap.computeIfAbsent(lockKey, k -> new ReentrantLock());
+
+        lock.lock();
+        try {
+            return generateDailyBillItemSerialNumberByDoctor(department, staff);
+        } finally {
+            lock.unlock();
+        }
     }
-    
-    //ByDoctorSession
-    public String fetchLastSerialNumberForDayUsingDoctorSession(Department department, Doctor doctor){
-        return "";
-    }
-    
-    
+
     // <editor-fold defaultstate="collapsed" desc="Find Session Number">
     private String generateDailyBillItemSerialNumberByBill(Department department) {
-        if(department == null){
+        if (department == null) {
             return "";
         }
 
@@ -155,7 +157,7 @@ public class SerialNumberGeneratorService {
         calEnd.set(Calendar.SECOND, 59);
         calEnd.set(Calendar.MILLISECOND, 999);
         Date endOfDay = calEnd.getTime();
-        
+
         String jpql = "SELECT count(b) FROM Bill b "
                 + " where b.createdAt between :fd and :td "
                 + " and b.department=:dep ";
@@ -169,12 +171,12 @@ public class SerialNumberGeneratorService {
 
         return (dd != null) ? String.valueOf(dd) : "0";
     }
-    
+
     private String generateDailyBillItemSerialNumberByCategory(Department department, Category category) {
-        if(department == null){
+        if (department == null) {
             return "";
         }
-        if(category == null){
+        if (category == null) {
             return "";
         }
 
@@ -191,7 +193,7 @@ public class SerialNumberGeneratorService {
         calEnd.set(Calendar.SECOND, 59);
         calEnd.set(Calendar.MILLISECOND, 999);
         Date endOfDay = calEnd.getTime();
-        
+
         String jpql = "SELECT count(bi) FROM BillItem bi "
                 + " where bi.bill.billDate between :fd and :td "
                 + " and bi.bill.department=:dep "
@@ -207,12 +209,12 @@ public class SerialNumberGeneratorService {
 
         return (dd != null) ? String.valueOf(dd) : "0";
     }
-    
+
     private String generateDailyBillItemSerialNumberByItem(Department department, Item item) {
-        if(department == null){
+        if (department == null) {
             return "";
         }
-        if(item == null){
+        if (item == null) {
             return "";
         }
 
@@ -229,7 +231,7 @@ public class SerialNumberGeneratorService {
         calEnd.set(Calendar.SECOND, 59);
         calEnd.set(Calendar.MILLISECOND, 999);
         Date endOfDay = calEnd.getTime();
-        
+
         String jpql = "SELECT count(bi) FROM BillItem bi "
                 + " where bi.bill.billDate between :fd and :td "
                 + " and bi.bill.department=:dep "
@@ -245,12 +247,12 @@ public class SerialNumberGeneratorService {
 
         return (dd != null) ? String.valueOf(dd) : "0";
     }
-    
+
     private String generateDailyBillItemSerialNumberByItemDepartment(Department department, Department itemDepartment) {
-        if(department == null){
+        if (department == null) {
             return "";
         }
-        if(itemDepartment == null){
+        if (itemDepartment == null) {
             return "";
         }
 
@@ -267,7 +269,7 @@ public class SerialNumberGeneratorService {
         calEnd.set(Calendar.SECOND, 59);
         calEnd.set(Calendar.MILLISECOND, 999);
         Date endOfDay = calEnd.getTime();
-        
+
         String jpql = "SELECT count(bi) FROM BillItem bi "
                 + " where bi.bill.billDate between :fd and :td "
                 + " and bi.bill.department=:dep "
@@ -276,6 +278,44 @@ public class SerialNumberGeneratorService {
 
         hm.put("dep", department);
         hm.put("itDep", itemDepartment);
+        hm.put("fd", startOfDay);
+        hm.put("td", endOfDay);
+
+        Long dd = billItemFacade.findAggregateLong(jpql, hm, TemporalType.TIMESTAMP);
+
+        return (dd != null) ? String.valueOf(dd) : "0";
+    }
+    
+    private String generateDailyBillItemSerialNumberByDoctor(Department department, Staff staff) {
+        if (department == null) {
+            return "";
+        }
+        if (staff == null) {
+            return "";
+        }
+
+        Calendar calStart = Calendar.getInstance();
+        calStart.set(Calendar.HOUR_OF_DAY, 0);
+        calStart.set(Calendar.MINUTE, 0);
+        calStart.set(Calendar.SECOND, 0);
+        calStart.set(Calendar.MILLISECOND, 0);
+        Date startOfDay = calStart.getTime();
+
+        Calendar calEnd = Calendar.getInstance();
+        calEnd.set(Calendar.HOUR_OF_DAY, 23);
+        calEnd.set(Calendar.MINUTE, 59);
+        calEnd.set(Calendar.SECOND, 59);
+        calEnd.set(Calendar.MILLISECOND, 999);
+        Date endOfDay = calEnd.getTime();
+
+        String jpql = "SELECT count(bi) FROM BillItem bi "
+                + " where bi.bill.billDate between :fd and :td "
+                + " and bi.bill.department=:dep "
+                + " and bi.primaryStaff=:sff ";
+        HashMap hm = new HashMap();
+
+        hm.put("dep", department);
+        hm.put("sff", staff);
         hm.put("fd", startOfDay);
         hm.put("td", endOfDay);
 
