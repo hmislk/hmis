29d529b8c63053182ad329981c59b35ad95dc1a6
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2026-03-31 12:38:30 +0530

    Fix request-originated float transfers not appearing in receive list
    When a FundTransferBill was created from a Float Transfer Request,
    referenceBill was set to the request bill. Since the entire system
    uses referenceBill=null to mean "pending" and referenceBill!=null to
    mean "accepted", this caused the transfer to be invisible in the
    to-receive list, incorrectly blocked from cancellation/decline, and
    excluded from shift-end drawer accumulation.
    
    Fix: remove setReferenceBill(requestBill) from
    navigateToFundTransferBillFromRequest(). The link back to the request
    is tracked via selectedFundTransferRequest (instance variable) and
    forwardReferenceBill on the request bill. Also clear
    selectedFundTransferRequest in both reset methods to prevent stale
    state bleeding into subsequent direct transfers.
    
    Closes #19603

--- src/main/java/com/divudi/bean/cashTransaction/FinancialTransactionController.java
+++ src/main/java/com/divudi/bean/cashTransaction/FinancialTransactionController.java
@@ -2302,11 +2302,10 @@ public class FinancialTransactionController implements Serializable {
                 return "/cashier/index?faces-redirect=true";
             }
         }
-        selectedFundTransferRequest = requestBill;
         resetClassVariablesWithoutSelectedBill();
+        selectedFundTransferRequest = requestBill;
         prepareToAddNewFundTransferBill();
         currentBill.setToWebUser(requestBill.getFromWebUser());
-        currentBill.setReferenceBill(requestBill);
         currentBill.setComments(requestBill.getComments());
         floatTransferStarted = false;
         currentBillPayments = new ArrayList<>();
@@ -2473,6 +2472,7 @@ public class FinancialTransactionController implements Serializable {
         paymentsFromShiftSratToNow = null;
         fundTransferAvailablePayments = null;
         department = null;
+        selectedFundTransferRequest = null;
         searchController.setBills(null);
     }
 
@@ -2499,6 +2499,7 @@ public class FinancialTransactionController implements Serializable {
         nonClosedShiftStartFundBill = null;
         paymentsFromShiftSratToNow = null;
         fundTransferAvailablePayments = null;
+        selectedFundTransferRequest = null;
     }
 
     public List<Payment> findPaymentsForBill(Bill b) {
@@ -2990,11 +2991,9 @@ public class FinancialTransactionController implements Serializable {
         currentBill.getPayments().addAll(currentBillPayments);
         billController.save(currentBill);
         // If this transfer was initiated from a float transfer request, mark the request as fulfilled
-        if (currentBill.getReferenceBill() != null
-                && currentBill.getReferenceBill().getBillTypeAtomic() == BillTypeAtomic.FUND_TRANSFER_REQUEST) {
-            Bill requestBill = currentBill.getReferenceBill();
-            requestBill.setForwardReferenceBill(currentBill);
-            billController.save(requestBill);
+        if (selectedFundTransferRequest != null) {
+            selectedFundTransferRequest.setForwardReferenceBill(currentBill);
+            billController.save(selectedFundTransferRequest);
         }
         floatTransferStarted = false;
         return "/cashier/fund_transfer_bill_print?faces-redirect=true";
