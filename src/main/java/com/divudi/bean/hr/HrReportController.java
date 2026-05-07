caaeb957d3ce808188f5f76ac05252b70bb846dc
RohanSGX <rohanonfirealways@gmail.com>
2026-05-07 16:51:03 +0530

    Include leave records in staff shift queries
    Uncomment leave type condition in StaffShift query to include shifts
    where staff are on leave, in addition to shifts with recorded start
    and end timestamps.

--- src/main/java/com/divudi/bean/hr/HrReportController.java
+++ src/main/java/com/divudi/bean/hr/HrReportController.java
@@ -2147,9 +2147,11 @@ public class HrReportController implements Serializable {
                 + " from StaffShift ss "
                 + " where ss.retired=false"
                 + " and ss.staff=:stf "
-                + " and ((ss.startRecord.recordTimeStamp is not null "
-                + " and ss.endRecord.recordTimeStamp is not null)) "
-                //                + " or (ss.leaveType is not null) ) "
+                + " and ("
+                + "   (ss.startRecord.recordTimeStamp is not null "
+                + "   and ss.endRecord.recordTimeStamp is not null)"
+                + "   or ss.leaveType is not null"
+                + " )"
                 + " and ss.shiftDate between :frm  and :to ";
         hm.put("frm", fd);
         hm.put("to", td);
