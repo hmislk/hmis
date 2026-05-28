e503d114b6124d2ab31bbe1c287828313eca63a4
damithdeshan98 <hkddrajapaksha@gmail.com>
2026-05-28 20:39:17 +0530

    Fixed - Accept Patient Deposit. Signed-off-by: damithdeshan98 <hkddrajapaksha@gmail.com>

--- src/main/java/com/divudi/bean/common/PatientDepositController.java
+++ src/main/java/com/divudi/bean/common/PatientDepositController.java
@@ -9,6 +9,7 @@
 package com.divudi.bean.common;
 
 import com.divudi.bean.cashTransaction.DrawerController;
+import com.divudi.bean.cashTransaction.FinancialTransactionController;
 import com.divudi.core.util.JsfUtil;
 import com.divudi.bean.opd.OpdBillController;
 import com.divudi.bean.report.ReportController;
@@ -75,6 +76,8 @@ public class PatientDepositController implements Serializable, ControllerWithPat
     ConfigOptionApplicationController configOptionApplicationController;
     @Inject
     OpdBillController opdBillController;
+    @Inject
+    private FinancialTransactionController financialTransactionController;
 
     @EJB
     PatientFacade patientFacade;
@@ -111,6 +114,13 @@ public class PatientDepositController implements Serializable, ControllerWithPat
 
     public String navigateToAddNewPatientDeposit() {
         clearDataForPatientDeposit();
+        financialTransactionController.findNonClosedShiftStartFundBillIsAvailable();
+        if (financialTransactionController.getNonClosedShiftStartFundBill() == null) {
+            // Use Flash scope to preserve error message across redirect
+            FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+            JsfUtil.addErrorMessage("Start Your Shift First !");
+            return "/cashier/index?faces-redirect=true";
+        }
         return "/patient_deposit/receive?faces-redirect=true";
     }
 
