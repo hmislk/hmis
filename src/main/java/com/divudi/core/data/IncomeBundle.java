c449a1071bb9d9c90d33a565324eb9396ae9f65c
PasinduW99 <pwathsara@gmail.com>
2026-08-11 14:55:07 +0530

    Signed-off-by: PasinduW99 <pwathsara@gmail.com>

--- src/main/java/com/divudi/core/data/IncomeBundle.java
+++ src/main/java/com/divudi/core/data/IncomeBundle.java
@@ -149,6 +149,7 @@ public class IncomeBundle implements Serializable {
         double sumOfVoucherValues = 0.0;
         double sumOfIouValues = 0.0;
         double sumOfAgentValues = 0.0;
+        double sumOfCcValues = 0.0;
         double sumOfChequeValues = 0.0;
         double sumOfSlipValues = 0.0;
         double sumOfEWalletValues = 0.0;
@@ -186,6 +187,7 @@ public class IncomeBundle implements Serializable {
             sumOfNoneValues += r.getNoneValue();
             sumOfOpdCreditValues += r.getOpdCreditValue();
             sumOfInpatientCreditValues += r.getInpatientCreditValue();
+            sumOfCcValues += r.getBill().getTotalCenterFee();
 
             sumOfGrossTotal += r.getGrossTotal();
             sumOfDiscount += r.getDiscount();
@@ -214,6 +216,7 @@ public class IncomeBundle implements Serializable {
         getSummaryRow().setNoneValue(sumOfNoneValues);
         getSummaryRow().setOpdCreditValue(sumOfOpdCreditValues);
         getSummaryRow().setInpatientCreditValue(sumOfInpatientCreditValues);
+        getSummaryRow().setCcTotal(sumOfCcValues);
 
         getSummaryRow().setGrossTotal(sumOfGrossTotal);
         getSummaryRow().setDiscount(sumOfDiscount);
