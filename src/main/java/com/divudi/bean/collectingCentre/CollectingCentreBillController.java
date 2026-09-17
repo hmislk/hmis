8a159f829862225a242dec49f79882e07afb9916
damithdeshan98 <dev3@carecode.lk>
2026-09-17 13:19:23 +0530

    feat(collecting-centre): add self-service bill print page after settle
    Self-service settle previously reused the staff bill_print page (full
    menu template), so settling a CC self-billed bill dropped the
    self-service user onto a page they can't otherwise reach. Add a
    dedicated cc_self_bill_print.xhtml on the self-service template and
    route settleCcBill() there for self-service, keeping the staff flow on
    bill_print.

--- src/main/java/com/divudi/bean/collectingCentre/CollectingCentreBillController.java
+++ src/main/java/com/divudi/bean/collectingCentre/CollectingCentreBillController.java
@@ -265,6 +265,8 @@ public class CollectingCentreBillController implements Serializable, ControllerW
 
     private final AtomicBoolean ccBillSettlingStarted = new AtomicBoolean(false);
 
+    private boolean ccSelfServiceBilling;
+
     public List<AgentReferenceBook> getAgentReferenceBooks() {
         return agentReferenceBooks;
     }
@@ -904,8 +906,12 @@ public class CollectingCentreBillController implements Serializable, ControllerW
         checkBillValues();
 
         ccBillSettlingStarted.set(false);
-        return "/collecting_centre/bill_print?faces-redirect=true";
-
+        if (ccSelfServiceBilling) {
+            return "/collecting_centre/cc_self_bill_print?faces-redirect=true";
+        }else{
+            return "/collecting_centre/bill_print?faces-redirect=true";
+        }
+        
     }
 
     public BillItem saveCcBillItem(Bill b, BillEntry e, WebUser wu) {
@@ -1709,6 +1715,7 @@ public class CollectingCentreBillController implements Serializable, ControllerW
         prepareNewBill();
         setPatient(getPatient());
         ccBillSettlingStarted.set(false);
+        ccSelfServiceBilling = false;
         return "/collecting_centre/bill?faces-redirect=true";
     }
 
@@ -1717,6 +1724,7 @@ public class CollectingCentreBillController implements Serializable, ControllerW
         fillAvailableAgentReferanceNumbers(collectingCentre);
         setPatient(getPatient());
         ccBillSettlingStarted.set(false);
+        ccSelfServiceBilling = false;
         return "/collecting_centre/bill?faces-redirect=true";
     }
 
@@ -1747,6 +1755,7 @@ public class CollectingCentreBillController implements Serializable, ControllerW
         fillAvailableAgentReferanceNumbers(collectingCentre);
         setPatient(getPatient());
         ccBillSettlingStarted.set(false);
+        ccSelfServiceBilling = false;
         return "/collecting_centre/bill?faces-redirect=true";
     }
 
@@ -1769,6 +1778,7 @@ public class CollectingCentreBillController implements Serializable, ControllerW
         itemController.setCcInstitutionItems(itemController.fillItemsByInstitution(collectingCentre));
         setPatient(getPatient());
         ccBillSettlingStarted.set(false);
+        ccSelfServiceBilling = true;
         return "/collecting_centre/cc_self_bill?faces-redirect=true";
     }
 
@@ -1777,6 +1787,7 @@ public class CollectingCentreBillController implements Serializable, ControllerW
         fillAvailableAgentReferanceNumbers(collectingCentre);
         setPatient(getPatient());
         ccBillSettlingStarted.set(false);
+        ccSelfServiceBilling = true;
         return "/collecting_centre/cc_self_bill?faces-redirect=true";
     }
 
